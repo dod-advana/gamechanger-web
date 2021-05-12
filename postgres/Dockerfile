f53ddd6676dafa88eccadf2e28807ca243f3725b
Jared Ramsey <ramsey_jared@bah.com>
2021-05-12 12:13:51 -0400

    Automating database creation and sequelize migrations

---
+++ postgres/Dockerfile
@@ -0,0 +1,3 @@
+FROM postgres:9.6
+# RUN mkdir /docker-entrypoint-initdb.d
+COPY ./init/* /docker-entrypoint-initdb.d/
---
+++ postgres/init/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE game_changer;
+	CREATE DATABASE "gc-orchestration";
+EOSQL
