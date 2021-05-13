7238d4573b3296bc7268b3458542d9cbca9ea290
Jared Ramsey <ramsey_jared@bah.com>
2021-05-13 11:41:51 -0400

    working on local elasticsearch

--- backend/start.sh
+++ backend/start.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+# if [ ! -f ./metadata/es_loaded ]; then
+# 	sleep 30 && curl -X PUT "elasticsearch:9200/gamechanger?pretty"
+# 	touch ./metadata/es_loaded
+# fi
 
 if [ ! -f ./metadata/dbs_created ]; then
-	echo "sleeping!"
     sleep 30 && \
 	sequelize --options-path ./.sequelize-game_changer --env game_changer db:migrate --url "postgresql://postgres:password@postgres/game_changer" && \
 	sequelize --options-path ./.sequelize-gc-orchestration --env gc_orchestration db:migrate --url "postgresql://postgres:password@postgres/gc-orchestration" && \
 	touch ./metadata/dbs_created && \
 	nodemon index.js
 else
-	echo "Not sleeping!"
+	sleep 10 && \
 	sequelize --options-path ./.sequelize-game_changer --env game_changer db:migrate --url "postgresql://postgres:password@postgres/game_changer" && \
 	sequelize --options-path ./.sequelize-gc-orchestration --env gc_orchestration db:migrate --url "postgresql://postgres:password@postgres/gc-orchestration" && \
 	touch ./metadata/dbs_created && \
