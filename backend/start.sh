aab16ffdfe43a673dccf4da6ac4d7e13663089ee
Jared Ramsey <ramsey_jared@bah.com>
2021-05-19 09:31:34 -0400

    Adding sleep for now

--- backend/start.sh
+++ backend/start.sh
@@ -1,19 +1,7 @@
-#!/bin/bash
-# if [ ! -f ./metadata/es_loaded ]; then
-# 	sleep 30 && curl -X PUT "elasticsearch:9200/gamechanger?pretty"
-# 	touch ./metadata/es_loaded
-# fi
 
-if [ ! -f ./metadata/dbs_created ]; then
-    sleep 30 && \
-	sequelize --options-path ./.sequelize-game_changer --env game_changer db:migrate --url "postgresql://postgres:password@postgres/game_changer" && \
-	sequelize --options-path ./.sequelize-gc-orchestration --env gc_orchestration db:migrate --url "postgresql://postgres:password@postgres/gc-orchestration" && \
-	touch ./metadata/dbs_created && \
-	nodemon index.js
-else
-	sleep 10 && \
-	sequelize --options-path ./.sequelize-game_changer --env game_changer db:migrate --url "postgresql://postgres:password@postgres/game_changer" && \
-	sequelize --options-path ./.sequelize-gc-orchestration --env gc_orchestration db:migrate --url "postgresql://postgres:password@postgres/gc-orchestration" && \
-	touch ./metadata/dbs_created && \
-	nodemon index.js
-fi
+#!/bin/bash
+
+sleep 30 && \
+sequelize --options-path ./.sequelize-game_changer --env game_changer db:migrate --url "postgresql://postgres:password@postgres/game_changer" && \
+sequelize --options-path ./.sequelize-gc-orchestration --env gc_orchestration db:migrate --url "postgresql://postgres:password@postgres/gc-orchestration" && \
+nodemon index.js
