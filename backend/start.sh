eba58adb31e0bf37dd71b5502f87af508bb212f1
Jared Ramsey <ramsey_jared@bah.com>
2021-05-24 12:34:00 -0400

    Adding more dev nice things

--- backend/start.sh
+++ backend/start.sh
@@ -4,5 +4,6 @@
 sleep 45 && \
 sequelize --options-path ./.sequelize-game_changer --env game_changer db:migrate --url "postgresql://postgres:password@postgres/game_changer" && \
 sequelize --options-path ./.sequelize-gc-orchestration --env gc_orchestration db:migrate --url "postgresql://postgres:password@postgres/gc-orchestration" && \
-psql -U postgres -h postgres -d game_changer -f /usr/src/app/node_app/sql/v89.0_insert_game_changer_clone_meta.sql && \
+psql -U postgres -h postgres -d game_changer -f /usr/src/app/node_app/init/create_clones.sql && \
+psql -U postgres -h postgres -d game_changer -f /usr/src/app/node_app/init/create_admins.sql && \
 nodemon index.js
