f988ca836031107973f38950dc0646c24e12d2be
Jared Ramsey <ramsey_jared@bah.com>
2021-05-24 12:07:22 -0400

    Making dev stuff smoother

--- backend/start.sh
+++ backend/start.sh
@@ -1,7 +1,8 @@
 
 #!/bin/bash
 
-sleep 30 && \
+sleep 45 && \
 sequelize --options-path ./.sequelize-game_changer --env game_changer db:migrate --url "postgresql://postgres:password@postgres/game_changer" && \
 sequelize --options-path ./.sequelize-gc-orchestration --env gc_orchestration db:migrate --url "postgresql://postgres:password@postgres/gc-orchestration" && \
+psql -U postgres -h postgres -d game_changer -f /usr/src/app/node_app/sql/v89.0_insert_game_changer_clone_meta.sql && \
 nodemon index.js
