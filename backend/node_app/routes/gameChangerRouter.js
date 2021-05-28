7cfcc13563c9a7cd8d1b3523e5d60fbfb37f924c
Sam Malagon <sam.malagon15@gmail.com>
2021-05-27 21:16:53 -0700

    add app setting

--- backend/node_app/routes/gameChangerRouter.js
+++ backend/node_app/routes/gameChangerRouter.js
@@ -135,6 +135,8 @@ router.get('/appSettings/intelligentAnswers', appSettings.getIntelligentAnswersM
 router.post('/appSettings/intelligentAnswers', appSettings.setIntelligentAnswersMode);
 router.get('/appSettings/entitySearch', appSettings.getEntitySearchMode);
 router.post('/appSettings/entitySearch', appSettings.setEntitySearchMode);
+router.get('/appSettings/userFeedback', appSettings.getUserFeedbackMode);
+router.post('/appSettings/userFeedback', appSettings.toggleUserFeedbackMode);
 
 router.post('/sendFeedback/intelligentSearch', feedback.sendIntelligentSearchFeedback);
 router.post('/sendFeedback/QA', feedback.sendQAFeedback);
