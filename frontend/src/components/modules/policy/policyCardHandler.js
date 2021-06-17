e77b7a885b80604f74c884c8df086449da01c15d
joshua.lyons <lyons_joshua@bah.com>
2021-06-17 12:43:13 -0500

    Fixed open url for cards

--- frontend/src/components/modules/policy/policyCardHandler.js
+++ frontend/src/components/modules/policy/policyCardHandler.js
@@ -419,7 +419,7 @@ const clickFn = (filename, cloneName, searchText, pageNumber = 0, sourceUrl) => 
     trackEvent(getTrackingNameForFactory(cloneName), 'CardInteraction' , 'PDFOpen');
 	trackEvent(getTrackingNameForFactory(cloneName), 'CardInteraction', 'filename', filename);
 	trackEvent(getTrackingNameForFactory(cloneName), 'CardInteraction', 'pageNumber', pageNumber);
-	window.open(`/#/pdfviewer/gamechanger?filename=${filename}${searchText ? `&prevSearchText=${searchText.replace(/"/gi, '')}` : null}&pageNumber=${pageNumber}&cloneIndex=${cloneName}${sourceUrl ? `&sourceUrl=${sourceUrl}` :null}`);
+	window.open(`/#/pdfviewer/gamechanger?filename=${filename}${searchText ? `&prevSearchText=${searchText.replace(/"/gi, '')}` : null}&pageNumber=${pageNumber}&cloneIndex=${cloneName}${sourceUrl ? `&sourceUrl=${sourceUrl}` : ''}`);
 };
 
 const addFavoriteTopicToMetadata = (data, userData, setFavoriteTopic, setFavorite, handleFavoriteTopicClicked, cloneData) => {
