f11b464ee298e3fe446f555c9e9f3aa95378d03b
britton-jason <Britton_Jason@bah.com>
2021-07-06 14:44:46 -0400

    Remove console log

--- frontend/src/components/modules/policy/policyMainViewHandler.js
+++ frontend/src/components/modules/policy/policyMainViewHandler.js
@@ -159,7 +159,6 @@ const PolicyMainViewHandler = {
 		if (!iframePreviewLink) sideScroll = {};
 		const cacheTip = `Cached result from ${timeSinceCache>0 ? timeSinceCache + " hour(s) ago": "less than an hour ago"}`;
 
-		console.log({docsPagination});
 		return (
 			<div key={'cardView'}>
 				<div key={'cardView'} style={{marginTop: hideTabs ? 40 : 'auto'}}>
