8b50ca80764660a6c290c9e54bdc547dbc04fec1
Brandon Herzog <herzog_brandon@bah.com>
2021-07-07 11:42:23 -0400

    be more careful about nuking volumes in reset script

--- resetDocker.sh
+++ resetDocker.sh
@@ -1,5 +1,23 @@
+#!/usr/bin/env bash
+
+VOLUMES=0
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    --volumes)
+      VOLUMES=1
+      shift # past argument
+      ;;
+    *)    # unknown option
+      shift # past argument
+      ;;
+  esac
+done
+
 docker-compose down
 docker system prune -f
-docker system prune --volumes -f
+if (($VOLUMES)); then
+  docker system prune --volumes -f
+fi
 docker-compose build
 docker-compose up
