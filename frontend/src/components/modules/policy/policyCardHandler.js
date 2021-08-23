154bc943763113734ddf32343244dd24e5f0108b
Krishna <manne_vikaskrishna@bah.com>
2021-08-23 09:58:41 -0700

    made item.name lower case

--- frontend/src/components/modules/policy/policyCardHandler.js
+++ frontend/src/components/modules/policy/policyCardHandler.js
@@ -1475,9 +1475,9 @@ const PolicyCardHandler = {
 					<>
 						<CardButton target={'_blank'} style={{...styles.footerButtonBack, CARD_FONT_SIZE}} href={'#'}
 							onClick={(e) => {
-								trackEvent(getTrackingNameForFactory(cloneName), 'TopicCardOnClick', 'Open', `${item.name}DetailsPage`);
+								trackEvent(getTrackingNameForFactory(cloneName), 'TopicCardOnClick', 'Open', `${item.name.toLowerCase()}DetailsPage`);
 								e.preventDefault();
-								window.open(`#/gamechanger-details?type=topic&topicName=${item.name}&cloneName=${cloneName}`);
+								window.open(`#/gamechanger-details?type=topic&topicName=${item.name.toLowerCase()}&cloneName=${cloneName}`);
 							}}
 						>
 							Open
