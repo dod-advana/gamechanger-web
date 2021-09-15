a284bbcee385391ff8459c7949b937e5fcc57c04
Brandon Herzog <herzog_brandon@bah.com>
2021-09-15 18:43:41 -0400

    implementing own simple redis lock so that we can differentiate error conditions from lock contention

--- backend/node_app/lib/cronJobs.js
+++ backend/node_app/lib/cronJobs.js
@@ -1,12 +1,11 @@
 const cron = require('node-cron');
-const redis = require('redis');
-const Redlock = require('redlock');
 const { CacheController } = require('../controllers/cacheController');
 const { FavoritesController } = require('../controllers/favoritesController');
 const { UserController } = require('../controllers/userController');
 const constantsFile = require('../config/constants');
 const LOGGER = require('../lib/logger');
 const { poll } = require('../utils/pollUtility');
+const { RedisLock, ResourceLockedError } = require('../utils/redisLock');
 
 class CronJobs {
 	constructor(opts = {}){
@@ -16,17 +15,7 @@ class CronJobs {
 			favoritesController = new FavoritesController(),
 			userController = new UserController(),
 			logger = LOGGER,
-			redisClient,
 		} = opts;
-
-		if (!redisClient){
-			this.redisClient = redis.createClient(process.env.REDIS_URL || 'redis://localhost');
-		}
-		
-		this.redlock = new Redlock([this.redisClient], {
-			// we're polling so no reason to retry
-			retryCount: 1,
-		});
 
 		this.constants = constants;
 		this.cacheController = cacheController;
@@ -41,24 +30,28 @@ class CronJobs {
 	init(){
 		this.cacheController.setStartupSearchHistoryCacheKeys();
 
-		const favoriteSearchPollInterval = this.constants.GAME_CHANGER_OPTS.favoriteSearchPollInterval;
+		const resource = 'locks:checkLeastRecentFavoritedSearch';
+		const favoriteSearchPollInterval = parseInt(this.constants.GAME_CHANGER_OPTS.favoriteSearchPollInterval, 10);
+		const ttl = Math.max(favoriteSearchPollInterval, 60000); // timeout lock after at least 60s
+		const redisLock = new RedisLock(resource, {ttl});
 		if (favoriteSearchPollInterval >= 0) {
 			this.logger.info(`Polling for favorite search updates enabled every ${favoriteSearchPollInterval}ms.`);
 			poll(async () => {
-				let lock;
+				let locked = false;
 				try {
-					const resource = 'locks:checkLeastRecentFavoritedSearch';
-					const ttl = Math.max(favoriteSearchPollInterval, 60000); // timeout lock after at least 60s
-					lock = await this.redlock.lock(resource, ttl);
+					await redisLock.lock();
+					locked = true;
 					await this.favoritesController.checkLeastRecentFavoritedSearch();
 				} catch (err) {
-					// XXX: this error is expected when the lock hasn't expired -- can we differentiate?
-					this.logger.error(err, 'M8HFXEH', 'this probably is not an issue');
+					// we expect the resource to sometimes already be locked
+					if (!(err instanceof ResourceLockedError)) {
+						this.logger.error(err, 'M8HFXEH');
+					}
 				} finally {
-					if (lock) {
+					if (locked) {
 						try {
 							// reduce the lock timeout to the poll interval
-							await lock.extend(favoriteSearchPollInterval);
+							await redisLock.extend(favoriteSearchPollInterval);
 						} catch (err) {
 							this.logger.error(err, 'N7FGETF');
 						}
