5a5079f787897b6ab0cafd1e1154478f718e91ea
Brandon Herzog <herzog_brandon@bah.com>
2021-09-16 14:17:22 -0400

    refactoring out distributed poll utility function

--- backend/node_app/utils/pollUtility.js
+++ backend/node_app/utils/pollUtility.js
@@ -1,3 +1,6 @@
+const { RedisLock, ResourceLockedError } = require('../utils/redisLock');
+const LOGGER = require('../lib/logger');
+
 async function poll(promiseFn, ms) {
 	ms = Math.max(0, ms);
 	while (true) {
@@ -6,4 +9,36 @@ async function poll(promiseFn, ms) {
 	}
 }
 
-module.exports = { poll };
+// this version of polling uses a lock in Redis to ensure that only
+// one instance of the application runs the function at any given time
+async function distributedPoll(lockName, promiseFn, ms, lockCheckMs = 1000) {
+	const ttl = Math.max(Math.ceil(ms), 60000); // timeout lock after at least 60s
+	const redisLock = new RedisLock(lockName, {ttl});
+	poll(async () => {
+		let locked = false;
+		try {
+			await redisLock.lock();
+			locked = true;
+			await promiseFn();
+		} catch (err) {
+			// we expect the resource to sometimes already be locked
+			if (!(err instanceof ResourceLockedError)) {
+				LOGGER.error(err, 'M8HFXEH');
+			}
+		} finally {
+			if (locked) {
+				try {
+					// reduce the lock timeout to the poll interval
+					await redisLock.extend(ms);
+				} catch (err) {
+					LOGGER.error(err, 'N7FGETF');
+				}
+			}
+		}
+	}, lockCheckMs); // we check every so often if we can acquire the lock
+}
+
+module.exports = { 
+	poll,
+	distributedPoll,
+};
