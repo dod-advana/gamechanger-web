c68a965619fd6edc9ef224280e01bd39e60f031c
Ryan Chan <ryanchan1622@gmail.com>
2021-10-04 09:35:18 -0700

    updated UI

--- frontend/src/components/admin/SearchPdfMapping.js
+++ frontend/src/components/admin/SearchPdfMapping.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import ReactTable from 'react-table';
 import XLSX from 'xlsx';
+import { Typography } from '@material-ui/core';
+import { Tabs, Tab, TabPanel, TabList } from 'react-tabs';
+import TabStyles from '../common/TabStyles';
 import GameChangerAPI from '../api//gameChanger-service-api';
 import GCPrimaryButton from '../common/GCButton';
 import { trackEvent } from '../telemetry/Matomo';
@@ -25,7 +28,6 @@ const TableRow = styled.div`
 
 const filterCaseInsensitiveIncludes = (filter, row) =>{
 	const id = filter.pivotId || filter.id;
-	console.log(row[id]);
 	return (
 		row[id] !== undefined ?
 			String(row[id].toLowerCase()).includes(filter.value.toLowerCase())
@@ -179,7 +181,8 @@ const getFeedbackData = async (setFeedbackData) => {
  */
 const getDocumentData = async (daysBack, setDocumentData) => {
 	try {
-		const {data = {} } = await gameChangerAPI.getDocumentUsage();
+		const params = { daysBack };
+		const {data = {} } = await gameChangerAPI.getDocumentUsage(params);
 		setDocumentData(data.data);
 	} catch (e) {
 		console.error(e);
@@ -198,6 +201,7 @@ export default () => {
 	const [feedbackData, setFeedbackData] = useState([]);
 	const [documentData, setDocumentData] = useState([]);
 	const [daysBack, setDaysBack] = useState(3);
+	const [tabIndex, setTabIndex] = useState('pdfMapping');
 
 	// flags that parameters have been changed and on
 	// blur or enter press we should update the query
@@ -230,7 +234,17 @@ export default () => {
 		}
 		if (shouldUpdate) {
 			setShouldUpdate(false);
-			getSearchPdfMapping(daysBack, setMappingData);
+			switch (tabIndex){
+				case 'pdfMapping':
+					getSearchPdfMapping(daysBack, setMappingData);
+					break;
+				case 'userTracking':
+					getDocumentData(daysBack, setDocumentData);
+					break;
+				default:
+					getSearchPdfMapping(daysBack, setMappingData);
+					getDocumentData(daysBack, setDocumentData);
+			}
 		}
 	};
 	/**
@@ -266,101 +280,210 @@ export default () => {
 	};
 
 	useEffect(() => {
-		getSearchPdfMapping(daysBack, setMappingData);
-		getFeedbackData(setFeedbackData);
-		getDocumentData(daysBack, setDocumentData);
-	}, [daysBack]);
+		switch (tabIndex){
+			case 'pdfMapping':
+				getSearchPdfMapping(daysBack, setMappingData);
+				break;
+			case 'userTracking':
+				getDocumentData(daysBack, setDocumentData);
+				break;
+			default:
+				getSearchPdfMapping(daysBack, setMappingData);
+				getFeedbackData(setFeedbackData);
+				getDocumentData(daysBack, setDocumentData);
+		}
+	}, [daysBack,tabIndex]);
+
+
 
 	return (
-		<div>
+		<div style={{ ...TabStyles.tabContainer, minHeight: 'calc(100vh-100px)' }}>
 			<div
 				style={{
 					display: 'flex',
 					justifyContent: 'space-between',
-					margin: '10px 80px',
+					padding: '10px 80px',
+					position: 'absolute',
+					right: '0px',
 				}}
 			>
 				<p style={{ ...styles.sectionHeader, marginLeft: 0, marginTop: 10 }}>
-					Search PDF Mapping: Document Opened Given Search
+					Machine Learning API
 				</p>
-
-				<CreateWrapper>
-					<LabelStack>
-						<label
-							htmlFor="daysBack"
-							style={{ marginRight: '10px', marginTop: '4px' }}
+			</div>
+			<Tabs>
+				<div
+					style={{
+						...TabStyles.tabButtonContainer,
+						background: 'rgb(245 245 245)',
+					}}
+				>
+					<TabList style={TabStyles.tabsList}>
+						<Tab
+							style={{
+								...TabStyles.tabStyle,
+								...(tabIndex === 'pdfMapping' ? TabStyles.tabSelectedStyle : {}),
+								borderRadius: `5px 0 0 0`,
+							}}
+							title="pdfMapping"
+							onClick={() => {
+								setTabIndex('pdfMapping');
+								setDaysBack(3);
+								}
+							}						>
+							<Typography variant="h6" display="inline">
+								Search PDF Mapping
+							</Typography>
+						</Tab>
+						<Tab
+							style={{
+								...TabStyles.tabStyle,
+								...(tabIndex === 'feedback' ? TabStyles.tabSelectedStyle : {}),
+								borderRadius: '0 0 0 0',
+							}}
+							title="feedback"
+							onClick={() => setTabIndex('feedback')}
 						>
-							Days Back:
-						</label>
-						<input
-							name="daysBack"
-							value={daysBack}
-							onChange={handleDaysBackChange}
-							onBlur={handleBlur}
-							onKeyPress={handlePositionEnter}
-						/>
-					</LabelStack>
-				</CreateWrapper>
-				<GCPrimaryButton
-					onClick={() => {
-						trackEvent('GAMECHANGER', 'ExportSearchPDFMapping', 'onClick');
-						exportMapping();
-					}}
-					style={{ minWidth: 'unset' }}
-				>
-					Export Mapping
-				</GCPrimaryButton>
-			</div>
+							<Typography variant="h6" display="inline">
+								Feedback
+							</Typography>
+						</Tab>
+						<Tab
+							style={{
+								...TabStyles.tabStyle,
+								...(tabIndex === 'userTracker' ? TabStyles.tabSelectedStyle : {}),
+								borderRadius: `0 0 0 0`,
+							}}
+							title="userTracker"
+							onClick={() => {
+								setTabIndex('userTracker');
+								setDaysBack(30);
+								}
+							}
+						>
+							<Typography variant="h6" display="inline">
+								User Tracker Tools
+							</Typography>
+						</Tab>
+					</TabList>
 
-			<ReactTable
-				data={mappingData}
-				columns={columns}
-				style={{ margin: '0 80px 20px 80px', height: 700 }}
-				defaultSorted={[{ id: 'searchtime', desc: true }]}
-			/>
-			<div
-				style={{
-					display: 'flex',
-					justifyContent: 'space-between',
-					margin: '10px 80px',
-				}}
-			>
-				<p style={{ ...styles.sectionHeader, marginLeft: 0, marginTop: 10 }}>
-					Feedback Data
-				</p>
-				<GCPrimaryButton
-					onClick={() => {
-						trackEvent('GAMECHANGER', 'ExportFeedback', 'onClick');
-						exportFeedback();
-					}}
-					style={{ minWidth: 'unset' }}
-				>
-					Export Feedback
-				</GCPrimaryButton>
-			</div>
-			<ReactTable
-				data={feedbackData}
-				columns={feedbackColumns}
-				style={{ margin: '0 80px 20px 80px', height: 700 }}
-				defaultSorted={[{ id: 'event_name', desc: false }]}
-			/>
-			<div style={{display: 'flex', justifyContent: 'space-between', margin: '10px 80px'}}>
-				<p style={{...styles.sectionHeader, marginLeft: 0, marginTop: 10}}>Document Usage Data</p>
-				<GCPrimaryButton
-					onClick={() => {
-						trackEvent('GAMECHANGER', 'ExportDocumentUsage', 'onClick');
-							
-					}}
-					style={{minWidth: 'unset'}}
-				>Export Document Usage</GCPrimaryButton>
-			</div>
-			<ReactTable
-				data={documentData}
-				columns={documentUsageColumns}
-				filterable={true}
-				style={{margin: '0 80px 20px 80px', height: 700}}
-				defaultSorted = {[ { id: 'visit_count', desc: true } ]}
-			/>
+					<div style={TabStyles.spacer} />
+				</div>
+
+				<div style={TabStyles.panelContainer}>
+					<TabPanel>
+						<div
+							style={{
+								display: 'flex',
+								justifyContent: 'space-between',
+								margin: '10px 80px',
+							}}
+						>
+							<p style={{ ...styles.sectionHeader, marginLeft: 0, marginTop: 10 }}>
+								Documents Opened Given Search
+							</p>
+
+							<CreateWrapper>
+								<LabelStack>
+									<label
+										htmlFor="daysBack"
+										style={{ marginRight: '10px', marginTop: '4px' }}
+									>
+										Days Back:
+									</label>
+									<input
+										name="daysBack"
+										value={daysBack}
+										onChange={handleDaysBackChange}
+										onBlur={handleBlur}
+										onKeyPress={handlePositionEnter}
+									/>
+								</LabelStack>
+							</CreateWrapper>
+							<GCPrimaryButton
+								onClick={() => {
+									trackEvent('GAMECHANGER', 'ExportSearchPDFMapping', 'onClick');
+									exportMapping();
+								}}
+								style={{ minWidth: 'unset' }}
+							>
+								Export Mapping
+							</GCPrimaryButton>
+						</div>
+
+						<ReactTable
+							data={mappingData}
+							columns={columns}
+							style={{ margin: '0 80px 20px 80px', height: 700 }}
+							defaultSorted={[{ id: 'searchtime', desc: true }]}
+						/>		
+					</TabPanel>
+					<TabPanel>
+						<div
+							style={{
+								display: 'flex',
+								justifyContent: 'space-between',
+								margin: '10px 80px',
+							}}
+						>
+							<p style={{ ...styles.sectionHeader, marginLeft: 0, marginTop: 10 }}>
+								Feedback Data
+							</p>
+							<GCPrimaryButton
+								onClick={() => {
+									trackEvent('GAMECHANGER', 'ExportFeedback', 'onClick');
+									exportFeedback();
+								}}
+								style={{ minWidth: 'unset' }}
+							>
+								Export Feedback
+							</GCPrimaryButton>
+						</div>
+						<ReactTable
+							data={feedbackData}
+							columns={feedbackColumns}
+							style={{ margin: '0 80px 20px 80px', height: 700 }}
+							defaultSorted={[{ id: 'event_name', desc: false }]}
+						/>
+					</TabPanel>
+					<TabPanel>
+						<div style={{display: 'flex', justifyContent: 'space-between', margin: '10px 80px'}}>
+							<p style={{...styles.sectionHeader, marginLeft: 0, marginTop: 10}}>Document Usage Data</p>
+							<CreateWrapper>
+								<LabelStack>
+									<label
+										htmlFor="daysBack"
+										style={{ marginRight: '10px', marginTop: '4px' }}
+									>
+										Days Back:
+									</label>
+									<input
+										name="daysBack"
+										value={daysBack}
+										onChange={handleDaysBackChange}
+										onBlur={handleBlur}
+										onKeyPress={handlePositionEnter}
+									/>
+								</LabelStack>
+							</CreateWrapper>
+							{/* <GCPrimaryButton
+								onClick={() => {
+									trackEvent('GAMECHANGER', 'ExportDocumentUsage', 'onClick');
+										
+								}}
+								style={{minWidth: 'unset'}}
+							>Export Document Usage</GCPrimaryButton> */}
+						</div>
+						<ReactTable
+							data={documentData}
+							columns={documentUsageColumns}
+							filterable={true}
+							style={{margin: '0 80px 20px 80px', height: 700}}
+							defaultSorted = {[ { id: 'visit_count', desc: true } ]}
+						/>
+					</TabPanel>
+				</div>
+			</Tabs>
 		</div>
 	);
 };
