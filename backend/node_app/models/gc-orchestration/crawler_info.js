acb0365608d8de0026a77343bc8eeccf4cab079e
taylorhernandez <hernandez_taylor@bah.com>
2021-10-08 13:04:52 -0400

    updated json to expect source fields data source s and source title when retrieving from postgres table

--- backend/node_app/models/gc-orchestration/crawler_info.js
+++ backend/node_app/models/gc-orchestration/crawler_info.js
@@ -3,10 +3,10 @@
 module.exports = (sequelize, DataTypes) => {
 	const Admin = sequelize.define('crawler_info',
 		{
-      crawler: {
-				type: DataTypes.STRING,
-				primaryKey: true,
-				allowNull: false
+			crawler: {
+			type: DataTypes.STRING,
+			primaryKey: true,
+			allowNull: false
 			},
 			display_org: {
 				type: DataTypes.STRING,
@@ -16,14 +16,22 @@ module.exports = (sequelize, DataTypes) => {
 				type: DataTypes.STRING,
 				allowNull: false
 			},
-      url_origin: {
+			source_title: {
 				type: DataTypes.STRING,
 				allowNull: false
 			},
-      image_link: {
+			url_origin: {
 				type: DataTypes.STRING,
 				allowNull: false
 			},
+			image_link: {
+				type: DataTypes.STRING,
+				allowNull: false
+			},
+			data_source_s: {
+				type: DataTypes.STRING,
+				allowNull: false
+			}		
 		},{
 			freezeTableName: true,
 			tableName: 'crawler_info',
