438bd07a2c2ee2befa1364d4d970750d29d16bf5
604840 <rha930@gmail.com>
2021-11-18 12:00:42 -0500

    adding correct check for title, removing old code, moving a check for suggestions so it doesnt print an error

--- backend/node_app/controllers/textSuggestionController.js
+++ backend/node_app/controllers/textSuggestionController.js
@@ -31,52 +31,53 @@ class TextSuggestionController {
 
 			const index = req.body.index ? req.body.index : this.constants.GAME_CHANGER_OPTS.index;
 			// const data = await this.dataApi.getTextSuggestion({ ...req.body, index }, userId);
+			if (req.body.searchText.length > 3){
+				const data = await this.textSuggestData({ ...req.body, index }, userId);
+				let corrected;
+				try {
+					corrected = this.getSingleCorrected(data.suggest.suggester);
+				} catch (err) {
+					const { message } = err;
+					this.logger.error(message, 'JBVZKTP', userId);
+				}
+				if (corrected.length > 0){
+					req.body.searchText = corrected;
+				}
+				const data_presearch = await this.getPresearchSuggestion({ ...req.body, index }, userId);
+				
+				let presearchTitle;
+				try {
+					presearchTitle = this.getPreTitleCorrected(data_presearch.responses[0].hits.hits);
+				} catch (err) {
+					const { message } = err;
+					this.logger.error(message, 'JBVZKTF', userId);
+				}
+	
+				let presearchHistory;
+				try {
+					presearchHistory = this.getPreHistoryCorrected(data_presearch.responses[1].aggregations.search_query.buckets);
+				} catch (err) {
+					const { message } = err;
+					this.logger.error(message, 'JBVZKTG', userId);
+				}
+				// for future use
+				// let predictions = [];
+				let presearchEntity;
+				try {
+					presearchEntity = this.getPreEntityCorrected(data_presearch.responses[2].hits.hits);
+				} catch (err) {
+					const { message } = err;
+					this.logger.error(message, 'JBVZKTF', userId);
+				}
+				return res.send({
+					autocorrect: corrected ? [corrected] : [],
+					presearchTitle: presearchTitle || [],
+					presearchTopic: presearchEntity.presearchTopic || [],
+					presearchOrg: presearchEntity.presearchOrg || [],
+					predictions: presearchHistory || []
+				});
+			}
 
-			const data = await this.textSuggestData({ ...req.body, index }, userId);
-
-			let corrected;
-			try {
-				corrected = this.getSingleCorrected(data.suggest.suggester);
-			} catch (err) {
-				const { message } = err;
-				this.logger.error(message, 'JBVZKTP', userId);
-			}
-			if (corrected.length > 0){
-				req.body.searchText = corrected;
-			}
-			const data_presearch = await this.getPresearchSuggestion({ ...req.body, index }, userId);
-			
-			let presearchTitle;
-			try {
-				presearchTitle = this.getPreTitleCorrected(data_presearch.responses[0].hits.hits);
-			} catch (err) {
-				const { message } = err;
-				this.logger.error(message, 'JBVZKTF', userId);
-			}
-
-			let presearchHistory;
-			try {
-				presearchHistory = this.getPreHistoryCorrected(data_presearch.responses[1].aggregations.search_query.buckets);
-			} catch (err) {
-				const { message } = err;
-				this.logger.error(message, 'JBVZKTG', userId);
-			}
-			// for future use
-			// let predictions = [];
-			let presearchEntity;
-			try {
-				presearchEntity = this.getPreEntityCorrected(data_presearch.responses[2].hits.hits);
-			} catch (err) {
-				const { message } = err;
-				this.logger.error(message, 'JBVZKTF', userId);
-			}
-			return res.send({
-				autocorrect: corrected ? [corrected] : [],
-				presearchTitle: presearchTitle || [],
-				presearchTopic: presearchEntity.presearchTopic || [],
-				presearchOrg: presearchEntity.presearchOrg || [],
-				predictions: presearchHistory || []
-			});
 
 		} catch (err) {
 			const { message } = err;
@@ -90,9 +91,7 @@ class TextSuggestionController {
 		try {
 			const esQuery = this.searchUtility.getESSuggesterQuery(body);
 
-			let esClientName = 'gamechanger';
-			// let esIndex = 'gamechanger';
-			
+			let esClientName = 'gamechanger';			
 
 			const results = await this.dataApi.queryElasticSearch(esClientName, this.constants.GAME_CHANGER_OPTS.textSuggestIndex, esQuery, userId);
 			return results.body;
