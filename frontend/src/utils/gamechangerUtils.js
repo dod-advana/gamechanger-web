f86b1f5b524806042a6cec1dd51f9499a1a01ba9
604840 <rha930@gmail.com>
2022-01-12 09:48:32 -0500

    removing console log and comments

--- frontend/src/utils/gamechangerUtils.js
+++ frontend/src/utils/gamechangerUtils.js
@@ -303,44 +303,13 @@ export function numberWithCommas(x) {
 	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
 
-// const crawlerMapping = {
-// 	dod_issuances: 'WHS DoD Directives Division',
-// 	army_pubs: 'Army Publishing Directorate',
-// 	jcs_pubs: 'Joint Chiefs of Staff Library',
-// 	jcs_manual_uploads: 'Joint Chiefs of Staff',
-// 	dod_manual_uploads: 'Dept. of Defense',
-// 	army_manual_uploads: 'US Army',
-// 	ic_policies: 'Director of National Intelligence',
-// 	us_code: 'Office of the Law Revision Counsel',
-// 	ex_orders: 'Federal Register',
-// 	opm_pubs: 'OMB Publication',
-// 	air_force_pubs: 'Dept. of the Air Force E-Publishing',
-// 	marine_pubs: 'Marine Corps Publications Electronic Library',
-// 	secnav_pubs: 'Dept. of the Navy Issuances',
-// 	navy_reserves: 'U.S. Navy Reserve Publications',
-// 	navy_med_pubs: 'Navy Medicine Directives',
-// 	Bupers_Crawler: 'Bureau of Naval Personnel Instructions',
-// 	milpersman_crawler: 'Navy Personnel Command Instructions',
-// 	nato_stanag: 'NATO Publications',
-// 	fmr_pubs: 'DoD Financial Management Regulation',
-// 	legislation_pubs: 'Congressional Legislation',
-// 	Army_Reserve: 'U.S. Army Reserve Publications',
-// 	Memo: 'OSD Executive Executive Secretary',
-// 	dha_pubs: 'Military Health System',
-// 	jumbo_FAR: 'Federal Acquisition Regulation',
-// 	jumbo_DFAR: 'Defense Federal Acquisition Regulation',
-// 	National_Guard: 'National Guard Bureau Publications Library',
-// 	Coast_Guard: 'US Coast Guard Directives',
-// 	dfar_subpart_regs: 'Defense Federal Acquisition Regulation',
-// 	far_subpart_regs: 'Federal Acquisition Regulation',
-// 	Chief_National_Guard_Bureau_Instructions:
-// 		'National Guard Bureau Instructions',
-// };
+
 
 const crawlerMapping = {
 	dod_issuances: ['WHS DoD Directives Division', 'Dept. of Defense'],
 	army_pubs: ['Army Publishing Directorate', 'Dept. of the Army', 'US Army'],
 	jcs_pubs: ['Joint Chiefs of Staff Library', 'CJCS Directives Library', 'Joint Chiefs of Staff'],
+	// leaving in case these exist or added back
 	//jcs_manual_uploads: ['Joint Chiefs of Staff'],
 	//dod_manual_uploads: ['Dept. of Defense'],
 	//army_manual_uploads: ['US Army'],
@@ -371,10 +340,8 @@ const crawlerMapping = {
 	code_of_federal_regulations: ['Federal Accounting Standards Advisory Board']
 };
 export const invertedCrawlerMappingFunc = (item) => {
-	const inverted = {};
 	let crawler = "";
 	for(let key in crawlerMapping) {
-		console.log(crawlerMapping[key])
 		if (crawlerMapping[key].map(name => name.toLowerCase()).includes(item.toLowerCase())){
 			crawler = key
 			break;
