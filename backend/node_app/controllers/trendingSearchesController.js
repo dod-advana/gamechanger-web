b364d40e3773883012bb98d7c6fdb1a1361e6e37
604840 <rha930@gmail.com>
2022-01-21 13:34:40 -0500

    fixing blacklist

--- backend/node_app/controllers/trendingSearchesController.js
+++ backend/node_app/controllers/trendingSearchesController.js
@@ -23,7 +23,7 @@ class TrendingSearchesController {
 		this.searchUtility = searchUtility;
 
 		this.trendingSearchesPOST = this.trendingSearchesPOST.bind(this);
-		this.getTrendingBlacklist = this.getTrendingBlacklist.bind(this);
+		this.getTrendingBlacklist = this.getTrendingBlacklist.bind(this); 
 		this.setTrendingBlacklist = this.setTrendingBlacklist.bind(this);
 		this.deleteTrendingBlacklist = this.deleteTrendingBlacklist.bind(this);
 		this.getWeeklySearchCount = this.getWeeklySearchCount.bind(this);
@@ -37,34 +37,23 @@ class TrendingSearchesController {
 			userId = req.get('SSL_CLIENT_S_DN_CN');
 			const {
 				cloneData = {},
+				daysBack,
 			} = req.body;
-
-			let cloneNameSql = {clone_name: cloneData.clone_name};
-
-			let exclusionSql = {search: {$and: [
-				{$notLike: '%artificial intelligence%'},
-				{$notLike: '%pizza%'},
-				{$notLike: 'pizza'},
-				{$notLike: '%cyber%'},
-				{$notLike: '%military intelligence program%'},
-				{$notLike: '%"artificial intelligence" and president%'},
-				{$notLike: '%machine learning%'},
-			]}, run_at: {
-				$gte: sequelize.literal('NOW() - INTERVAL \'7d\''),
-			} };
-
-			const trending = await this.gcHistory.findAll({
+			let trending = [];
+			const blacklist = [];
+			const blacklistItems = await this.gcTrendingBlacklist.findAll({
 				attributes: [
-					sequelize.literal('trim(lower(search)) as cleaned_search, count(*) as val')
+					sequelize.literal('search_text, added_by, \"updatedAt\"')
 				],
-				where: exclusionSql, cloneNameSql,
-				group: ['cleaned_search'],
 				order: [
-					[sequelize.literal('val'), 'DESC']
+					[sequelize.literal('\"updatedAt\"'), 'DESC']
 				],
-				limit: 10,
 				raw: true
 			});
+			for (let items of blacklistItems) {
+				blacklist.push(items['search_text'])
+			 }
+			trending = await this.searchUtility.getSearchCount(daysBack, blacklist, userId)
 			res.status(200).send(trending);
 		} catch (err) {
 			this.logger.error(err, '5ED9CQB', userId);
@@ -73,6 +62,49 @@ class TrendingSearchesController {
 		}
 	}
 
+	// note: deprecated 1/21
+	// async trendingSearchesPOST_(req, res) {
+	// 	let userId = 'Unknown';
+
+	// 	try {
+	// 		userId = req.get('SSL_CLIENT_S_DN_CN');
+	// 		const {
+	// 			cloneData = {},
+	// 		} = req.body;
+
+	// 		let cloneNameSql = {clone_name: cloneData.clone_name};
+
+	// 		let exclusionSql = {search: {$and: [
+	// 			{$notLike: '%artificial intelligence%'},
+	// 			{$notLike: '%pizza%'},
+	// 			{$notLike: 'pizza'},
+	// 			{$notLike: '%cyber%'},
+	// 			{$notLike: '%military intelligence program%'},
+	// 			{$notLike: '%"artificial intelligence" and president%'},
+	// 			{$notLike: '%machine learning%'},
+	// 		]}, run_at: {
+	// 			$gte: sequelize.literal('NOW() - INTERVAL \'7d\''),
+	// 		} };
+
+	// 		const trending = await this.gcHistory.findAll({
+	// 			attributes: [
+	// 				sequelize.literal('trim(lower(search)) as cleaned_search, count(*) as val')
+	// 			],
+	// 			where: exclusionSql, cloneNameSql,
+	// 			group: ['cleaned_search'],
+	// 			order: [
+	// 				[sequelize.literal('val'), 'DESC']
+	// 			],
+	// 			limit: 10,
+	// 			raw: true
+	// 		});
+	// 		res.status(200).send(trending);
+	// 	} catch (err) {
+	// 		this.logger.error(err, '5ED9CQB', userId);
+	// 		res.status(500).send(err);
+	// 		return err;
+	// 	}
+	// }
 	async getTrendingBlacklist(req, res) {
 		let userId = 'Unknown';
 
@@ -158,6 +190,9 @@ class TrendingSearchesController {
 			res.status(500).send(results);
 		}
 	}
+	
+	// note: deprecated 1/21
+
 	// async getWeeklySearchCount_(req, res) {
 	// 	let userId = 'Unknown';
 
