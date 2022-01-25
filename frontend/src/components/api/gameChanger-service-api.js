823967a5a5eb70c8054fecb36ee5131f1e9261c1
616836 <david.rospond@gmail.com>
2022-01-25 15:12:43 -0500

    split axiosPOST return based on cancelToken

--- frontend/src/components/api/gameChanger-service-api.js
+++ frontend/src/components/api/gameChanger-service-api.js
@@ -376,14 +376,22 @@ export default class GameChangerAPI {
 	thumbnailStorageDownloadPOST = async (filenames, folder, cloneData, cancelToken) => {
 		const s3Bucket = cloneData?.s3_bucket ?? 'advana-data-zone/bronze';
 		const url = endpoints.thumbnailStorageDownloadPOST;
+		if(cancelToken){
+			return axiosPOST(
+				this.axios,
+				url,
+				{ filenames, folder, clone_name: cloneData.clone_name, dest: s3Bucket },
+				{ 
+					timeout: 0,
+					cancelToken: cancelToken?.token ? cancelToken.token : {}
+				}
+			);
+		}
 		return axiosPOST(
 			this.axios,
 			url,
 			{ filenames, folder, clone_name: cloneData.clone_name, dest: s3Bucket },
-			{ 
-				timeout: 0,
-				cancelToken: cancelToken?.token ? cancelToken.token : {}
-			}
+			{ timeout: 0 }
 		);
 	};
 
