345cfa316f4861a9ebdf441de252f14e68143d3c
604840 <rha930@gmail.com>
2022-01-27 11:45:42 -0500

    adding assist index to gc

--- backend/node_app/modules/policy/policySearchHandler.js
+++ backend/node_app/modules/policy/policySearchHandler.js
@@ -143,7 +143,7 @@ class PolicySearchHandler extends SearchHandler {
 			esClientName: 'gamechanger',
 			esIndex: body.archivedCongressSelected ?
 				[this.constants.GAMECHANGER_ELASTIC_SEARCH_OPTS.index, this.constants.GAMECHANGER_ELASTIC_SEARCH_OPTS.legislation_index] :
-				this.constants.GAMECHANGER_ELASTIC_SEARCH_OPTS.index
+				[this.constants.GAMECHANGER_ELASTIC_SEARCH_OPTS.index, this.constants.GAMECHANGER_ELASTIC_SEARCH_OPTS.assist_index]
 		};
 
 		try {
