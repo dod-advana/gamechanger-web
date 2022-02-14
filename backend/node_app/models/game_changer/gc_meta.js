1519ddd147a0ac2988097b5e6165ef62c7ed5075
Ryan Chan <ryanchan1622@gmail.com>
2022-02-14 14:01:37 -0800

    jbook clone in postgres

--- backend/node_app/models/game_changer/gc_meta.js
+++ backend/node_app/models/game_changer/gc_meta.js
@@ -61,6 +61,9 @@ module.exports = (sequelize, DataTypes) => {
 			search_bar_module: {
 				type: DataTypes.STRING
 			},
+			data_module: {
+				type: DataTypes.STRING
+			},
 			s3_bucket: {
 				type: DataTypes.STRING
 			},
