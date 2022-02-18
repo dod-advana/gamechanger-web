a3093024a14dcf82a1bbbf8e7994149a7ef98c20
Joshua Lyons <lyons_joshua@bah.com>
2022-02-17 19:31:02 -0600

    Fixing backend issues

--- frontend/src/utils/axiosUtils.js
+++ frontend/src/utils/axiosUtils.js
@@ -3,8 +3,7 @@ const CryptoJS = require('crypto-js');
 const Base64 = require('crypto-js/enc-base64');
 
 export const getSignature = (options, url) => {
-	console.log(Auth.getToken())
-	const signature = Base64.stringify(
+	const signature = CryptoJS.enc.Base64.stringify(
 		CryptoJS.HmacSHA256(url, Auth.getToken())
 	);
 	options.headers = { 'X-UA-SIGNATURE': signature };
