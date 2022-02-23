e92b5c67a9e5888498dae139b9a4d056edcb7706
britton-jason <Britton_Jason@bah.com>
2022-02-23 10:44:19 -0500

    Simplify code

--- frontend/src/components/graph/defaultGraphView.js
+++ frontend/src/components/graph/defaultGraphView.js
@@ -173,9 +173,7 @@ const DefaultGraphView = (props) => {
 				searchText={state.searchText}
 				selectedDocuments={state.selectedDocumentsForGraph}
 				loadAll={() => {
-					const newSearchSettings = _.cloneDeep(state.searchSettings);
-					newSearchSettings.loadAll = true;
-					setState(dispatch, { searchSettings: newSearchSettings, runGraphSearch: true });
+					setState(dispatch, { searchSettings: { ...state.searchSettings, loadAll: true }, runGraphSearch: true });
 					setGraphResultsFound(false);
 				}}
 				nodeLimit={nodeLimit}
