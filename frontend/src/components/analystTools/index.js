668aa50b0a02df629060f63ecf5d0dae5732029a
616836 <david.rospond@gmail.com>
2022-03-01 11:21:42 -0500

    code clean up

--- frontend/src/components/analystTools/index.js
+++ frontend/src/components/analystTools/index.js
@@ -5,7 +5,7 @@ import {Tab, TabList, TabPanel, Tabs} from 'react-tabs';
 import propTypes from 'prop-types';
 import {trackEvent} from '../telemetry/Matomo';
 import {getTrackingNameForFactory} from '../../utils/gamechangerUtils';
-import GCDocumentsComparisonTool from './GCDocumentsComparisonToolNew';
+import GCDocumentsComparisonTool from './GCDocumentsComparisonTool';
 import {setState} from '../../utils/sharedFunctions';
 import GCResponsibilityExplorer from './GCResponsibilityExplorer';
 
