897cfe2cd7a58727d1870e3b03524d056130b0e5
Ryan Chan <ryanchan1622@gmail.com>
2022-04-14 11:06:24 -0700

    added modal

--- frontend/src/components/modules/jbook/jbookPortfolioBuilder.js
+++ frontend/src/components/modules/jbook/jbookPortfolioBuilder.js
@@ -3,6 +3,7 @@ import { styles } from '../../../components/admin/util/GCAdminStyles';
 import GCButton from '../../common/GCButton';
 import { Checkbox, FormControlLabel, FormGroup } from '@material-ui/core';
 import styled from 'styled-components';
+import JbookPortfolioModal from './jbookPortfolioModal';
 
 import GameChangerAPI from '../../api/gameChanger-service-api';
 
@@ -17,8 +18,10 @@ const portfolioStyles = {
 		width: '400px',
 	},
 	portfolioHeader: {
-		fontSize: '1.3em',
+		fontSize: '1.5em',
 		paddingBottom: '20px',
+		display: 'flex',
+		justifyContent: 'space-between',
 	},
 	pillbox: {
 		maxHeight: '100px',
@@ -38,6 +41,7 @@ const Pill = styled.button`
 	margin-right: 6px;
 	margin-bottom: 3px;
 	border: 1px solid rgb(209, 215, 220);
+	cursor: default !important;
 	> i {
 		margin-left: 3px;
 		color: #e9691d;
@@ -49,7 +53,10 @@ const Pill = styled.button`
  */
 const PortfolioBuilder = (props) => {
 	// State variables for the buttons
-	let [portfolios, setPortfolios] = useState([]);
+	const [portfolios, setPortfolios] = useState([]);
+	const [showModal, setShowModal] = useState(false);
+	const [modalData, setModalData] = useState({});
+
 	let [init, setInit] = useState(false);
 
 	useEffect(() => {
@@ -73,13 +80,28 @@ const PortfolioBuilder = (props) => {
 		let portfolios = pList.map((portfolio) => {
 			return (
 				<div style={portfolioStyles.portfolio}>
-					<div style={portfolioStyles.portfolioHeader}>{portfolio.name}</div>
+					<div style={portfolioStyles.portfolioHeader}>
+						<div>{portfolio.name}</div>
+						<div>
+							<GCButton
+								onClick={() => {
+									setModalData(portfolio);
+									setShowModal(true);
+								}}
+							>
+								Edit
+							</GCButton>
+						</div>
+						<div>
+							<GCButton onClick={() => {}}>Delete</GCButton>
+						</div>
+					</div>
 					<div style={{ fontSize: '.8em' }}>{portfolio.description}</div>
 					<hr />
 					<div style={portfolioStyles.portfolioHeader}>People With Access</div>
 					<div style={portfolioStyles.pillbox}>
 						{portfolio.user_ids.length === 0 &&
-							(portfolio.name === 'jaic' ? '(all jbook users)' : '(none)')}
+							(portfolio.name === 'AI Inventory' ? '(All JBOOK users)' : '(none)')}
 						{portfolio.user_ids.map((user, index) => {
 							return (
 								<Pill>
@@ -129,7 +151,12 @@ const PortfolioBuilder = (props) => {
             `}
 					</div>
 					<div style={{ flex: 1, display: 'flex', flexDirection: 'row-reverse' }}>
-						<GCButton onClick={async () => {}} style={{ minWidth: 'unset' }}>
+						<GCButton
+							onClick={() => {
+								setShowModal(true);
+							}}
+							style={{ minWidth: 'unset' }}
+						>
 							Create a New Portfolio
 						</GCButton>
 						<GCButton
@@ -195,6 +222,11 @@ const PortfolioBuilder = (props) => {
 					])}
 				</div>
 			</div>
+			<JbookPortfolioModal
+				showModal={showModal}
+				setShowModal={setShowModal}
+				modalData={modalData}
+			></JbookPortfolioModal>
 		</>
 	);
 };
