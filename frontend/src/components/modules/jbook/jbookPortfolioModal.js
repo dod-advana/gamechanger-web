6f2a6f6a7f216624240621820804fcbf1c0f7a3a
aaron42051 <aaron42051@gmail.com>
2022-04-29 10:28:24 -0700

    add user and tag modal (wip)

--- frontend/src/components/modules/jbook/jbookPortfolioModal.js
+++ frontend/src/components/modules/jbook/jbookPortfolioModal.js
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { Dialog, DialogActions, DialogContent, DialogTitle, Grid, TextField, Typography } from '@material-ui/core';
+import {
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
+	Grid,
+	TextField,
+	Typography,
+	FormControl,
+} from '@material-ui/core';
+import AddIcon from '@mui/icons-material/Add';
 import GCButton from '../../common/GCButton';
 import { styles, useStyles } from '../../admin/util/GCAdminStyles';
+import JbookAddUsersModal from './jbookAddUsersModal';
+import JbookAddTagsModal from './jbookAddTagsModal';
 import styled from 'styled-components';
 
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
-import CancelIcon from '@mui/icons-material/Cancel';
-import Autocomplete from '@material-ui/lab/Autocomplete';
 import GameChangerAPI from '../../api/gameChanger-service-api';
 const gameChangerAPI = new GameChangerAPI();
 
 const Pill = styled.button`
+	padding: 2px 10px 3px;
 	border: none;
 	border-radius: 15px;
 	background-color: white;
@@ -29,6 +40,7 @@ const Pill = styled.button`
 		color: #e9691d;
 	}
 `;
+
 /**
  *
  * @class UserModal
@@ -36,6 +48,9 @@ const Pill = styled.button`
 export default ({ showModal, setShowModal, modalData, userList, userMap }) => {
 	const classes = useStyles();
 	const [init, setInit] = useState(false);
+	const [showUsersModal, setShowUsersModal] = useState(false);
+	const [showTagsModal, setShowTagsModal] = useState(false);
+
 	const emptyData = {
 		name: '',
 		description: '',
@@ -50,10 +65,55 @@ export default ({ showModal, setShowModal, modalData, userList, userMap }) => {
 		setShowModal(false);
 		setData(emptyData);
 	};
+
 	const handleTextChange = (val, key) => {
 		const newData = { ...data };
 		newData[key] = val;
 		setData(newData);
+	};
+
+	const handleAddButton = (id) => {
+		if (!isNaN(id)) {
+			let newList = [...data.user_ids];
+			let idIndex = newList.indexOf(id);
+			if (idIndex !== -1) {
+				newList.splice(idIndex, 1);
+			} else {
+				newList.push(id);
+			}
+
+			handleTextChange(newList, 'user_ids');
+		}
+	};
+
+	const renderSelectedUsers = () => {
+		let selectedUsers = [];
+		let userIDs = data.user_ids;
+		for (let i = 0; i < userIDs.length; i++) {
+			let user = userMap[userIDs[i]];
+			selectedUsers.push(
+				<Pill id={userIDs[i]} style={{ margin: '0 5px 10px' }}>
+					{user.first_name} {user.last_name}
+					<IconButton
+						id={userIDs[i]}
+						aria-label="close"
+						style={{
+							backgroundColor: '#BDBDBD',
+							width: 17,
+							height: 17,
+							margin: '0 0 0 5px',
+							color: 'white',
+							padding: 0,
+						}}
+						onClick={() => handleAddButton(userIDs[i])}
+					>
+						<CloseIcon style={{ fontSize: 12 }} id={userIDs[i]} />
+					</IconButton>
+				</Pill>
+			);
+		}
+
+		return selectedUsers;
 	};
 
 	useEffect(() => {
@@ -70,162 +130,161 @@ export default ({ showModal, setShowModal, modalData, userList, userMap }) => {
 	 */
 
 	return (
-		<Dialog
-			open={showModal}
-			scroll={'paper'}
-			maxWidth="sm"
-			disableEscapeKeyDown
-			disableBackdropClick
-			classes={{
-				paperWidthSm: classes.dialogSm,
-			}}
-		>
-			<DialogTitle>
-				<div style={{ display: 'flex', width: '100%' }}>
-					<Typography variant="h3" display="inline" style={{ fontWeight: 700 }}>{`${
-						create ? 'Create' : 'Edit'
-					} Portfolio`}</Typography>
-				</div>
-				<IconButton
-					aria-label="close"
-					style={{
-						position: 'absolute',
-						right: '0px',
-						top: '0px',
-						height: 60,
-						width: 60,
-						color: 'black',
-						backgroundColor: styles.backgroundGreyLight,
-						borderRadius: 0,
-					}}
-					onClick={() => closeReviewerModal()}
-				>
-					<CloseIcon style={{ fontSize: 30 }} />
-				</IconButton>
-			</DialogTitle>
-			<DialogContent style={{ height: 500 }}>
-				<div style={{ margin: '0 20px' }}>
-					<Grid
-						container
-						style={{ background: '#f2f2f2', borderRadius: 6, marginTop: 10, marginBottom: 10, padding: 10 }}
+		<>
+			<JbookAddUsersModal
+				showModal={showUsersModal}
+				setShowModal={setShowUsersModal}
+				userList={userList}
+				portfolioData={data}
+				renderSelectedUsers={renderSelectedUsers}
+				handleAddButton={handleAddButton}
+			/>
+			<JbookAddTagsModal
+				showModal={showTagsModal}
+				setShowModal={setShowTagsModal}
+				tagsList={[]}
+				portfolioData={data}
+			/>
+			<Dialog
+				open={showModal}
+				scroll={'paper'}
+				maxWidth="sm"
+				disableEscapeKeyDown
+				disableBackdropClick
+				classes={{
+					paperWidthSm: classes.dialogSm,
+				}}
+			>
+				<DialogTitle>
+					<div style={{ display: 'flex', width: '100%' }}>
+						<Typography variant="h3" display="inline" style={{ fontWeight: 700 }}>{`${
+							create ? 'Create' : 'Edit'
+						} Portfolio`}</Typography>
+					</div>
+					<IconButton
+						aria-label="close"
+						style={{
+							position: 'absolute',
+							right: '0px',
+							top: '0px',
+							height: 60,
+							width: 60,
+							color: 'black',
+							backgroundColor: styles.backgroundGreyLight,
+							borderRadius: 0,
+						}}
+						onClick={() => closeReviewerModal()}
 					>
-						<Typography style={{ fontFamily: 'Montserrat', fontSize: 16 }}>
-							Complete the fields below to {create ? 'create a new' : 'edit'} portfolio
-						</Typography>
-					</Grid>
-					<Grid container>
-						<Grid item xs={12}>
-							<TextField
-								style={{ width: '100%', backgroundColor: 'white', marginTop: '10px' }}
-								label="Portfolio Name"
-								id="margin-dense"
-								variant="outlined"
-								defaultValue={data.name ?? null}
-								onChange={(event) => handleTextChange(event.target.value, 'name')}
-								className={classes.textField}
-								margin="dense"
-							/>
-							<TextField
-								style={{ width: '100%', backgroundColor: 'white', marginTop: '10px' }}
-								label="Portfolio Description"
-								id="margin-dense"
-								variant="outlined"
-								defaultValue={data.description ?? null}
-								onChange={(event) => handleTextChange(event.target.value, 'description')}
-								className={classes.textField}
-								margin="dense"
-								multiline
-								rows={4}
-							/>
-							{}
-							<Autocomplete
-								label="Users"
-								options={userList}
-								getOptionLabel={(user) =>
-									(user.first_name ? user.first_name : '') +
-									' ' +
-									(user.last_name ? user.last_name : '')
-								}
-								value={[]}
-								style={{ backgroundColor: 'white', marginTop: '10px' }}
-								renderInput={(params) => {
-									return <TextField {...params} label="Users" variant="outlined" />;
-								}}
-								onChange={(event, value) => {
-									const newList = [...data.user_ids, value.id];
-									console.log(newList);
-									handleTextChange(newList, 'user_ids');
-								}}
-							/>
-							<div style={{ marginTop: '10px' }}>
-								{data.user_ids.map((user, index) => {
-									return (
-										<Pill>
-											<div style={{ marginRight: '5px', marginLeft: '5px' }}>
-												{userMap[user].first_name + ' ' + userMap[user].last_name}
-											</div>
-											<IconButton
-												aria-label="close"
-												style={{
-													height: 10,
-													width: 10,
-													color: 'red',
-													borderRadius: 0,
-												}}
-												onClick={() => {
-													let newUsers = data.user_ids.filter((item) => item.id === user.id);
-													const newData = { ...data, user_ids: newUsers };
-													setData(newData);
-												}}
-											>
-												<CancelIcon style={{ fontSize: 30 }} />
-											</IconButton>
-										</Pill>
-									);
-								})}
-							</div>
-
-							<Autocomplete
-								label="Tags"
-								options={['Tag 1', 'Tag 2', 'Tag 3']}
-								value={data.tags ?? []}
-								style={{ backgroundColor: 'white', marginTop: '10px' }}
-								renderInput={(params) => <TextField {...params} label="Tags" variant="outlined" />}
-								onChange={(event, value) => {
-									console.log(value);
-									handleTextChange(value, 'tags');
-								}}
-								multiple
-							/>
+						<CloseIcon style={{ fontSize: 30 }} />
+					</IconButton>
+				</DialogTitle>
+				<DialogContent style={{ height: 500, padding: '8px 5px' }}>
+					<div style={{ margin: '0 20px' }}>
+						<Grid
+							container
+							style={{
+								background: '#f2f2f2',
+								borderRadius: 6,
+								marginTop: 10,
+								marginBottom: 10,
+								padding: 10,
+							}}
+						>
+							<Typography style={{ fontFamily: 'Montserrat', fontSize: 16 }}>
+								Complete the fields below to {create ? 'create a new' : 'edit'} portfolio
+							</Typography>
 						</Grid>
-					</Grid>
-				</div>
-			</DialogContent>
-			<DialogActions>
-				<GCButton
-					id={'editReviewerClose'}
-					onClick={closeReviewerModal}
-					style={{ margin: '10px' }}
-					buttonColor={'#8091A5'}
-				>
-					Close
-				</GCButton>
-				<GCButton
-					id={'editReviewerSubmit'}
-					onClick={async () => {
-						console.log(data);
-						const res = await gameChangerAPI.callDataFunction({
-							functionName: data.id === undefined ? 'createPortfolio' : 'editPortfolio',
-							cloneName: 'jbook',
-							options: { ...data },
-						});
-						closeReviewerModal();
-					}}
-					style={{ margin: '10px', backgroundColor: '#1C2D64', borderColor: '#1C2D64' }}
-				>
-					Submit
-				</GCButton>
-			</DialogActions>
-		</Dialog>
+						<Grid container>
+							<Grid item xs={12}>
+								<TextField
+									style={{ width: '100%', backgroundColor: 'white', marginTop: '10px' }}
+									label="Portfolio Name"
+									id="margin-dense"
+									variant="outlined"
+									defaultValue={data.name ?? null}
+									onChange={(event) => handleTextChange(event.target.value, 'name')}
+									className={classes.textField}
+									margin="dense"
+								/>
+								<TextField
+									style={{ width: '100%', backgroundColor: 'white', marginTop: '10px' }}
+									label="Portfolio Description"
+									id="margin-dense"
+									variant="outlined"
+									defaultValue={data.description ?? null}
+									onChange={(event) => handleTextChange(event.target.value, 'description')}
+									className={classes.textField}
+									margin="dense"
+									multiline
+									rows={4}
+								/>
+								<hr />
+								<Typography variant="h5" display="inline" style={{ fontWeight: 700 }}>
+									ADD PEOPLE WITH ACCESS
+								</Typography>
+								<FormControl
+									fullWidth
+									sx={{ m: 1 }}
+									variant="standard"
+									style={{ marginTop: 15, display: 'flex', flexDirection: 'row' }}
+								>
+									<IconButton
+										aria-label="close"
+										style={{ backgroundColor: styles.backgroundGreyLight, marginRight: 15 }}
+										onClick={() => setShowUsersModal(true)}
+									>
+										<AddIcon style={{ cursor: 'pointer' }} />
+									</IconButton>
+									<div style={{ minHeight: 35 }}>{renderSelectedUsers()}</div>
+								</FormControl>
+								<hr />
+								<Typography variant="h5" display="inline" style={{ fontWeight: 700 }}>
+									ADD TAGS
+								</Typography>
+								<FormControl
+									fullWidth
+									sx={{ m: 1 }}
+									variant="standard"
+									style={{ marginTop: 15, display: 'flex', flexDirection: 'row' }}
+								>
+									<IconButton
+										aria-label="close"
+										style={{ backgroundColor: styles.backgroundGreyLight }}
+										onClick={() => setShowTagsModal(true)}
+									>
+										<AddIcon style={{ cursor: 'pointer' }} />
+									</IconButton>
+									<div style={{ minHeight: 35 }}></div>
+								</FormControl>
+							</Grid>
+						</Grid>
+					</div>
+				</DialogContent>
+				<DialogActions>
+					<GCButton
+						id={'editReviewerClose'}
+						onClick={closeReviewerModal}
+						style={{ margin: '10px' }}
+						buttonColor={'#8091A5'}
+					>
+						Close
+					</GCButton>
+					<GCButton
+						id={'editReviewerSubmit'}
+						onClick={async () => {
+							const res = await gameChangerAPI.callDataFunction({
+								functionName: data.id === undefined ? 'createPortfolio' : 'editPortfolio',
+								cloneName: 'jbook',
+								options: { ...data },
+							});
+							closeReviewerModal();
+						}}
+						style={{ margin: '10px', backgroundColor: '#1C2D64', borderColor: '#1C2D64' }}
+					>
+						Submit
+					</GCButton>
+				</DialogActions>
+			</Dialog>
+		</>
 	);
 };
