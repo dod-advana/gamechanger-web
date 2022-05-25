a308d56a9f24aeff3eaee3ca86cb25d0d75117a6
Ryan Chan <ryanchan1622@gmail.com>
2022-05-25 15:14:03 -0700

    adds sort by relevance as default

--- frontend/src/components/modules/jbook/jbookContext.js
+++ frontend/src/components/modules/jbook/jbookContext.js
@@ -348,6 +348,7 @@ const initState = {
 	resetSettingsSwitch: false,
 	categorySorting: {
 		jbook: [
+			'Relevance',
 			'Budget Year',
 			'Program Element',
 			'Budget Line Item',
@@ -360,7 +361,7 @@ const initState = {
 			'Source',
 		],
 	},
-	currentSort: 'Budget Year',
+	currentSort: 'Relevance',
 	currentOrder: 'desc',
 	activeCategoryTab: 'jbook',
 
