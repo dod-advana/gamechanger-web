ef19904e3673708d8acdb0d0c9753415c5292216
616836 <david.rospond@gmail.com>
2022-05-31 15:55:36 -0400

    fixed wording on tutorial steps

--- frontend/src/components/analystTools/reTutotialSteps.js
+++ frontend/src/components/analystTools/reTutotialSteps.js
@@ -22,7 +22,7 @@ const reTutorialSteps = [
 		target: '.re-tutorial-step-1',
 		title: 'Filter the Results',
 		content:
-			'Filters allow you to narrow down your search by either document title, entity, or responsibility text.',
+			'Filters allow users to narrow down search results by either document title, entity, or responsibility text.',
 		disableScrolling: true,
 		clickButton: true,
 		buttonID: 'update-search',
@@ -33,7 +33,7 @@ const reTutorialSteps = [
 		title: 'Results',
 		disableScrolling: true,
 		content:
-			'The results section displays documents and responsibilities relevant to your filters. It is organized into a hierarchy of expandable sections with documents at the top level, followed by entities, and then by responsibilities listed for the entity.',
+			'The results section displays documents and responsibilities relevant to the selected filters. It is organized into a hierarchy of expandable sections with documents at the top level, followed by entities, and finally by responsibilities.',
 	},
 	{
 		...tutorialObject,
@@ -41,27 +41,27 @@ const reTutorialSteps = [
 		title: 'Report Data Issue',
 		disableScrolling: true,
 		content:
-			'Machine learning models are used to extract responsibilities from documents. In case something was not extracted correctly, you can click here to report the issue you see with this piece of data.',
+			'Machine learning models are used to extract responsibilities from documents. In case something was not extracted correctly, click here to report any issues with the data.',
 	},
 	{
 		...tutorialObject,
 		target: '.re-tutorial-step-4',
 		title: 'Document Viewer',
 		content:
-			"The document for the currently selected responsibility can be viewed here. The selected responsiblity's text and corresponding entity will be highlight in the document.",
+			"The document for the currently selected responsibility can be viewed in the center of the screen. The selected responsiblity's text and corresponding entity will be highlighted in the document.",
 	},
 	{
 		...tutorialObject,
 		target: '.re-tutorial-step-5',
 		title: 'Export Results',
-		content: 'Export the results of your search to CSV by selecting the orange export icon',
+		content: 'Export results to CSV by selecting the export icon.',
 	},
 	{
 		...tutorialObject,
 		target: '.re-tutorial-step-6',
 		title: 'Change the View',
 		content:
-			"The view dropdown allows you to switch between viewing the results in the default 'Document View' and viewing the results in a table format in the 'Chart View'.",
+			'The view dropdown allows users to switch from the default "Document View" to a table format in the "Chart View"',
 
 		clickButton: true,
 		buttonID: 'set-re-view',
@@ -70,7 +70,7 @@ const reTutorialSteps = [
 		...tutorialObject,
 		target: '.re-tutorial-step-7',
 		title: 'Chart View',
-		content: 'In the chart view, you can see results displayed in a simplified table format.',
+		content: "In the 'Chart View', results are displayed in a simplified table format.",
 	},
 ];
 
