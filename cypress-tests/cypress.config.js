a1b12fa1eb334f733b575ee8d9dfe3a4e8e5c1a9
Joshua Lyons <lyons_joshua@bah.com>
2022-06-26 09:07:58 -0500

    Putting cypress in its on folder with its on package lock to spee dup testing times.

--- cypress-tests/cypress.config.js
+++ cypress-tests/cypress.config.js
@@ -1,15 +1,15 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-	chromeWebSecurity: false,
-	e2e: {
-		excludeSpecPattern: 'cypress/e2e/examples/**/*.cy.js',
-	},
-	reporter: 'mochawesome',
-	reporterOptions: {
-		reportDir: 'cypress/results',
-		overwrite: false,
-		html: false,
-		json: true,
-	},
+  chromeWebSecurity: false,
+  e2e: {
+    excludeSpecPattern: "cypress/e2e/examples/**/*.cy.js",
+  },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/results",
+    overwrite: false,
+    html: false,
+    json: true,
+  },
 });
--- cypress-tests/cypress/e2e/advanaCommon/commonUI.cy.js
+++ cypress-tests/cypress/e2e/advanaCommon/commonUI.cy.js
@@ -1,49 +1,49 @@
-import CypressHelper from '../../support/CypressHelper';
+import CypressHelper from "../../support/CypressHelper";
 
-describe('Tests the advanaCommon functionality of the app including side nav and consent banner.', () => {
-	const cypressHelper = new CypressHelper(cy);
+describe("Tests the advanaCommon functionality of the app including side nav and consent banner.", () => {
+  const cypressHelper = new CypressHelper(cy);
 
-	beforeEach(() => {
-		// Setup headers
-		cypressHelper.setupHeaders();
+  beforeEach(() => {
+    // Setup headers
+    cypressHelper.setupHeaders();
 
-		// Clear cookies
-		cy.clearCookies();
+    // Clear cookies
+    cy.clearCookies();
 
-		// Visit the main page
-		cy.visit(Cypress.env('BASE_URL'));
+    // Visit the main page
+    cy.visit(Cypress.env("BASE_URL") + "/#/gamechanger");
 
-		// Click the okay button
-		cy.get('[data-cy="consent-agreement-okay"]').click();
-	});
+    // Click the okay button
+    cy.get('[data-cy="consent-agreement-okay"]').click();
+  });
 
-	// Test the consent banner appears and a cookie is created when okay clicked
-	it('The consent banner should be there.', () => {
-		// Clear cookies just for this one to test consent banner
-		cy.clearCookies();
+  // Test the consent banner appears and a cookie is created when okay clicked
+  it("The consent banner should be there.", () => {
+    // Clear cookies just for this one to test consent banner
+    cy.clearCookies();
 
-		// Visit the main page
-		cy.visit(Cypress.env('BASE_URL'));
+    // Visit the main page
+    cy.visit(Cypress.env("BASE_URL") + "/#/gamechanger");
 
-		// Check the consent banner is there
-		cy.get('[data-cy="consent-agreement"]').should('exist');
-	});
+    // Check the consent banner is there
+    cy.get('[data-cy="consent-agreement"]').should("exist");
+  });
 
-	it('The consent banner should be gone with a cookie showing agreement.', () => {
-		// Get the consent cookie
-		cy.getCookie('data.mil-consent-agreed').should('exist');
+  it("The consent banner should be gone with a cookie showing agreement.", () => {
+    // Get the consent cookie
+    cy.getCookie("data.mil-consent-agreed").should("exist");
 
-		// Check the consent banner is not there
-		cy.get('[data-cy="consent-agreement"]').should('not.exist');
-	});
+    // Check the consent banner is not there
+    cy.get('[data-cy="consent-agreement"]').should("not.exist");
+  });
 
-	// Tests the basic side navigation functionality
-	it('The side nav should be there.', () => {
-		cy.get('[data-cy="blah"]').should('exist');
-	});
+  // Tests the basic side navigation functionality
+  it("The side nav should be there.", () => {
+    cy.get('[data-cy="blah"]').should("exist");
+  });
 
-	// Tests the advana pills functionality
-	it('The advana pills should be there.', () => {
-		cy.get('[data-cy="blah2"]').should('exist');
-	});
+  // Tests the advana pills functionality
+  it("The advana pills should be there.", () => {
+    cy.get('[data-cy="blah2"]').should("exist");
+  });
 });
