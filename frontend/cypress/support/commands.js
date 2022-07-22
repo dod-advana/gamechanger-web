5878286119c5f344753a6456163dda95d80a0672
Melodie Butz <Butz_Melodie@bah.com>
2022-07-22 15:00:55 -0700

    add a basic jbook search e2e test

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -36,3 +36,37 @@
 //   }
 // }
 import '@testing-library/cypress/add-commands';
+import CypressHelper, { BASE_URL } from './CypressHelper';
+
+// this handles setting up your cookies and headers
+Cypress.Commands.add('setup', () => {
+	CypressHelper.setupHeaders(cy);
+	// Clear cookies
+	cy.clearCookies();
+});
+
+// this visits a page and clicks the consent agreement
+Cypress.Commands.add('visitPage', (page) => {
+	// Visit the main page
+	cy.visit(`${BASE_URL}/#/${page}`);
+
+	// Click the okay button
+	cy.get('[data-cy="consent-agreement-okay"]', { timeout: 10000 }).click();
+});
+
+//jbook commands
+
+// this visits the jbook page, clicks consent agreement, and
+// then waits for initial search tocomplete
+Cypress.Commands.add('initJbookVisit', () => {
+	cy.visitPage('jbook');
+
+	// Wait for search box to be visible
+	cy.get('#gcSearchInput', { timeout: 10000 }).should('exist');
+	// Wait for initial search to be done
+	cy.get('[data-cy="jbook-search-load"]', { timeout: 10000 }).should('not.exist');
+	// Wait for the results to be visible
+	cy.get('[data-cy="jbook-search-results"]', { timeout: 10000 }).should('exist');
+	// Results should have >1
+	cy.get('[data-cy="jbook-search-results"]').find('[data-cy="searchCard"]').should('have.length.greaterThan', 1);
+});
