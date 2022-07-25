37fd162c68598982af3c2da9e23ab2bb3542f13c
Melodie Butz <Butz_Melodie@bah.com>
2022-07-25 10:24:39 -0700

    add search to ensure service reviewer filter returns results

--- frontend/cypress/e2e/jbook/jbookSearches.cy.js
+++ frontend/cypress/e2e/jbook/jbookSearches.cy.js
@@ -28,4 +28,34 @@ describe('Tests multiple types of jbook searches.', () => {
 			.first()
 			.should('contain', 'BLI: 0206629M | Title: Amphibious Assault Vehicle');
 	});
+
+	it('service reviewer filter works', () => {
+		// switch to AI Inventory portfolio
+		cy.switch_portfolio('AI Inventory');
+
+		// open serviceReviewer filter accordion and check "Select specific" checkbox
+		cy.jbook_open_specific_filter('serviceReviewer');
+
+		// choose Misty Blower as a filter
+		const reviewer = 'Blowers, Misty (USMC US Marine Corp)';
+		cy.jbook_select_specific_filter_options([reviewer]);
+
+		// Wait for search to finish
+		cy.get('[data-cy="jbook-search-load"]', { timeout: 5000 }).should('not.exist');
+
+		// Wait for the results cards to have loaded
+		cy.scrollTo('top');
+		cy.get('[data-cy="jbook-card-header"]').should('exist');
+
+		// Results should have more than 1
+		cy.get('[data-cy="jbook-search-results"]')
+			.find('[data-cy="jbook-card-header"]')
+			.should('have.length.greaterThan', 1);
+
+		// First result should have the correct name
+		cy.get('[data-cy="jbook-search-results"]')
+			.find('[data-cy="jbook-card-header"]')
+			.first()
+			.should('contain', 'BLI: 0206623M | Title: Fire Support System');
+	});
 });
