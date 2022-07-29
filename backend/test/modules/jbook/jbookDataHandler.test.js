388e941ed15716f90451d4e96e3951fe41ca98b1
616836 <david.rospond@gmail.com>
2022-07-29 16:14:24 -0400

    added test for getUserSpecificReviews

--- backend/test/modules/jbook/jbookDataHandler.test.js
+++ backend/test/modules/jbook/jbookDataHandler.test.js
@@ -2,7 +2,13 @@ const assert = require('assert');
 const JBookDataHandler = require('../../../node_app/modules/jbook/jbookDataHandler');
 const { constructorOptionsMock } = require('../../resources/testUtility');
 
-const { keywordData, esData, portfolioData, reviewData } = require('../../resources/mockResponses/jbookMockData');
+const {
+	keywordData,
+	esData,
+	portfolioData,
+	reviewData,
+	userReviews,
+} = require('../../resources/mockResponses/jbookMockData');
 
 describe('JBookDataHandler', function () {
 	describe('#getProjectData', () => {
@@ -519,6 +525,58 @@ describe('JBookDataHandler', function () {
 			};
 			const actual = await target.getPortfolios(req, 'Test');
 			assert.deepStrictEqual(expected, actual);
+			done();
+		});
+	});
+
+	describe('#getUserSpecificReviews', () => {
+		it('should get all reviews for user', async (done) => {
+			const opts = {
+				...constructorOptionsMock,
+				constants: {
+					GAME_CHANGER_OPTS: { downloadLimit: 1000, allow_daterange: true },
+					GAMECHANGER_ELASTIC_SEARCH_OPTS: { index: 'Test', assist_index: 'Test' },
+					EDA_ELASTIC_SEARCH_OPTS: { index: 'Test', assist_index: 'Test' },
+				},
+				dataLibrary: {
+					queryElasticSearch() {
+						return Promise.resolve(userReviews);
+					},
+				},
+			};
+			const req = {
+				body: {
+					email: 'test@test.com',
+				},
+			};
+			const target = new JBookDataHandler(opts);
+			const expected = {
+				docs: [
+					{
+						appropriationNumber: '2040',
+						budgetActivityNumber: '03',
+						budgetLineItem: 'MO2',
+						budgetType: 'rdoc',
+						budgetYear: '2023',
+						createdAt: '2022-07-29T15:35:42.958Z',
+						hasKeywords: false,
+						id: 'rdoc#2023#PB#03#0603002A#21#N/A#2040#MO2',
+						keywords: [],
+						portfolioName: 'AI Inventory',
+						programElement: '0603002A',
+						projectNum: 'MO2',
+						projectTitle: 'Traumatic Brain Injury (TBI) Treatment Adv Tech',
+						reviewStatus: 'Needs Review',
+						serviceAgency: 'Army',
+						serviceReviewStatus: 'Partial Review',
+						serviceSecondaryReviewer: 'Test',
+						serviceSecondaryReviewerEmail: 'test@test.com',
+						updatedAt: '2022-07-29T15:35:42.958Z',
+					},
+				],
+			};
+			const actual = await target.getUserSpecificReviews(req, 'Test');
+			assert.deepStrictEqual(actual, expected);
 			done();
 		});
 	});
