5b5c1976f1a9643ad54b109d5e95bf062d2b7b15
britton-jason <Britton_Jason@bah.com>
2022-08-01 12:39:56 -0400

    Fix date pickers

--- frontend/src/components/modules/eda/edaSummaryView.js
+++ frontend/src/components/modules/eda/edaSummaryView.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import moment from 'moment';
 import GCButton from '../../common/GCButton';
 
