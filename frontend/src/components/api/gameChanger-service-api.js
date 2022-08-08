adaca5e12ff9904f2c89c07cfd1e03135b4ba3ed
Melodie Butz <Butz_Melodie@bah.com>
2022-08-08 08:57:20 -0700

    user feedback table pulls from jbook feedback table

--- frontend/src/components/api/gameChanger-service-api.js
+++ frontend/src/components/api/gameChanger-service-api.js
@@ -124,6 +124,7 @@ const endpoints = {
 	topicSearch: '/api/gamechanger/appSettings/topicSearch',
 	qaSearchFeedback: '/api/gamechanger/sendFeedback/QA',
 	getFeedbackData: '/api/gamechanger/sendFeedback/getFeedbackData',
+	getJbookFeedbackData: '/api/gamechanger/sendFeedback/getJbookFeedbackData',
 	sendFrontendErrorPOST: '/api/gamechanger/sendFrontendError',
 	getOrgImageOverrideURLs: '/api/gamechanger/getOrgImageOverrideURLs',
 	saveOrgImageOverrideURL: '/api/gamechanger/saveOrgImageOverrideURL',
@@ -1021,6 +1022,11 @@ export default class GameChangerAPI {
 
 	getFeedbackData = async () => {
 		const url = endpoints.getFeedbackData;
+		return axiosGET(this.axios, url);
+	};
+
+	getJbookFeedbackData = async () => {
+		const url = endpoints.getJbookFeedbackData;
 		return axiosGET(this.axios, url);
 	};
 
