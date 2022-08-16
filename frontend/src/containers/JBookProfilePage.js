f9794cf5c45a0eb022b58becfb1e80bccceabf3a
Ryan Chan <ryanchan1622@gmail.com>
2022-08-16 15:02:58 -0700

    some cleanup

--- frontend/src/containers/JBookProfilePage.js
+++ frontend/src/containers/JBookProfilePage.js
@@ -1,4 +1,6 @@
 import React, { useState, useContext, useEffect, useCallback } from 'react';
+import { trackEvent } from '../components/telemetry/Matomo';
+import { getTrackingNameForFactory, encode } from '../utils/gamechangerUtils';
 import GCAccordion from '../components/common/GCAccordion';
 import { Typography, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
@@ -99,10 +101,18 @@ const JBookProfilePage = () => {
 	const [budgetYearProjectData, setBudgetYearProjectData] = useState({});
 	let [init, setInit] = useState(false);
 
+	const clickFnPDF = (filename, cloneName, pageNumber = 0) => {
+		trackEvent(getTrackingNameForFactory(cloneName), 'CardInteraction', 'PDFOpen');
+		trackEvent(getTrackingNameForFactory(cloneName), 'CardInteraction', 'filename', filename);
+		trackEvent(getTrackingNameForFactory(cloneName), 'CardInteraction', 'pageNumber', pageNumber);
+		window.open(
+			`/#/pdfviewer/gamechanger?filename=${encode(filename)}&pageNumber=${pageNumber}&cloneIndex=${cloneName}`
+		);
+	};
+
 	useEffect(() => {
 		const getUserData = async () => {
 			const data = await gameChangerAPI.getUserData('jbook');
-			// const pdf = await gameChangerAPI.dataStorageDownloadGET()
 			const newMap = {};
 			data.data.users.forEach((user) => {
 				newMap[user.id] = user;
@@ -1355,19 +1365,47 @@ const JBookProfilePage = () => {
 					/>
 				</StyledMainContainer>
 				<StyledRightContainer>
-					<GCPrimaryButton
-						style={{
-							color: 'white',
-							backgroundColor: '#1C2D64',
-							borderColor: '#1C2D64',
-							height: '35px',
-							marginBottom: '15px',
-							marginLeft: '0',
-						}}
-						onClick={() => undefined}
-					>
-						OPEN DOCUMENT
-					</GCPrimaryButton>
+					{projectData.dtic_pdf_location_s && (
+						<GCPrimaryButton
+							style={{
+								color: 'white',
+								backgroundColor: '#1C2D64',
+								borderColor: '#1C2D64',
+								height: '35px',
+								marginBottom: '15px',
+								marginLeft: '0',
+							}}
+							onClick={() => {
+								console.log(projectData);
+								clickFnPDF(
+									projectData.dtic_pdf_location_s,
+									cloneData.clone_name,
+									projectData.dtic_pdf_page_s
+								);
+							}}
+						>
+							OPEN DOCUMENT
+						</GCPrimaryButton>
+					)}
+					{projectData.pdf_location_s && (
+						<GCPrimaryButton
+							style={{
+								color: 'white',
+								backgroundColor: '#1C2D64',
+								borderColor: '#1C2D64',
+								height: '35px',
+								marginBottom: '15px',
+								marginLeft: '0',
+							}}
+							onClick={() => {
+								console.log(projectData);
+								clickFnPDF(projectData.pdf_location_s, cloneData.clone_name);
+							}}
+						>
+							OPEN FULL PDF
+						</GCPrimaryButton>
+					)}
+
 					<Metadata
 						budgetType={budgetType}
 						projectNum={projectNum}
