39cb820b637c8820a84a0b64ea93f3d7587b2c14
parton_brandon <parton_brandon@bah.com>
2022-08-19 10:55:18 -0400

    User Dashboard Tests
    -Changed test user from Josh to a new Test User
    -New commands
    -Cypress files to allow Custom Cypress commands to show in intellisense(e2e.js, index.d.ts, tsconfig.js)
    -Bunch of data-cy tags

---
+++ frontend/cypress/support/index.d.ts
@@ -0,0 +1,78 @@
+declare namespace Cypress {
+	interface Chainable<Subject> {
+		/**
+		 * Sets up headers and clears cookies
+		 */
+		setup();
+
+		/**
+		 * Only accepts the consent banner
+		 */
+		accept_consent();
+
+		/**
+		 * Visits the given clone and accepts the DoD consent banner
+		 * @example cy.visit_accept_consent('Gamechanger')
+		 */
+		visit_accept_consent(clone: string);
+
+		/**
+		 * Custom command to select DOM element by data-cy attribute.
+		 * @example cy.getDataCy('greeting', {timeout: 10000})
+		 */
+		getDataCy(value: string, options?: any): Chainable<Element>;
+
+		/**
+		 * Custom command to select child DOM elements by data-cy attribute.
+		 * @example cy.getDataCy('greeting').findDataCy('card-button').click()
+		 */
+		findDataCy(value: string, options?: any): Chainable<Element>;
+
+		/**
+		 * Clears cookies, navigates to given clone, and accepts the consent banner
+		 * @example cy.login('Gamechanger')
+		 */
+		login(clone: string);
+
+		/**
+		 * Types into the search bar and clicks the search button.
+		 * Also waits up to 60 seconds for search results to appear.
+		 * @param searchTerm
+		 * @example cy.search('United States Army')
+		 */
+		search(searchTerm: string);
+
+		/**
+		 * Expands the Source accordion and selects a source filter from the sidebar
+		 */
+		setSourceFilter(sourceFilter: string);
+
+		/**
+		 * Expands the Type accordion and selects a type filter from the sidebar
+		 */
+		setTypeFilter(typeFilter: string);
+
+		/**
+		 * Grabs the base of a card at the given index. You will need to chain other cypress commands to do other tasks
+		 * such as, get the title, description, click open, etc... The example below will get the first card and then click the Open button on the card.
+		 * @param index
+		 * @param options
+		 * @example cy.getCard(1, {timeout: 10000}).find('a').contains('Open').click();
+		 */
+		getCard(index: number, options?: any): Chainable<Element>;
+
+		/**
+		 * Grabs the base element of a favorite card by its title
+		 * @param favoriteTitle
+		 * @param options
+		 */
+		getFavoriteCard(favoriteTitle: string, options?: any): Chainable<Element>;
+
+		/**
+		 * Switch between the tabs under the Search Bar (Organizations, Topics, Documents)
+		 * @param tabName
+		 * @example cy.switchTab('Organizations');
+		 */
+		switchTab(tabName: String);
+	}
+}
