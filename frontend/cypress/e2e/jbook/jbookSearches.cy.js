d41a272249851110aecdff933308faf148c41440
Melodie Butz <Butz_Melodie@bah.com>
2022-08-18 18:56:49 -0700

    test for search bar working from profile page

--- frontend/cypress/e2e/jbook/jbookSearches.cy.js
+++ frontend/cypress/e2e/jbook/jbookSearches.cy.js
@@ -1,8 +1,4 @@
 describe('Tests multiple types of jbook searches.', () => {
-	before(() => {
-		// cy.setup();
-	});
-
 	beforeEach(() => {
 		cy.setup();
 		cy.initial_jbook_visit();
@@ -54,5 +50,41 @@ describe('Tests multiple types of jbook searches.', () => {
 			.find('[data-cy="jbook-card-header"]')
 			.first()
 			.should('contain', 'BLI: 846070 | Title: DARP RC135');
+	});
+});
+
+describe.only('Tests search from multiple pages.', () => {
+	before(() => {
+		cy.setup();
+	});
+
+	it('can search from profile page', () => {
+		// load particular document's profile page
+		cy.visit_accept_consent(
+			'jbook/profile?type=Procurement&id=pdoc#2019#PB#05#A01000#57#N/A#3010&appropriationNumber=3010&portfolioName=General&budgetYear=2019'
+		);
+
+		// look for name of document in profile page title
+		cy.getDataCy('jbook-profile-title', { timeout: 15000 }).should('contain', 'A01000: A-10  Air Force (AF) ');
+
+		// Type in a PE to search
+		cy.get('#gcSearchInput').type('0206623M');
+
+		// Get the search button and click it
+		cy.get('#gcSearchButton').click();
+
+		// Wait for the results to be visible
+		cy.getDataCy('jbook-search-results', { timeout: 10000 }).should('exist');
+
+		// Results should have more than 1
+		cy.getDataCy('jbook-search-results')
+			.find('[data-cy="jbook-card-header"]', { timeout: 20000 })
+			.should('have.length.greaterThan', 1);
+
+		// First result should have the correct name
+		cy.getDataCy('jbook-search-results')
+			.find('[data-cy="jbook-card-header"]')
+			.first()
+			.should('contain', 'PE: 0206623M - MC Ground Cmbt Spt Arms Sys');
 	});
 });
