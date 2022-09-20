ae962537c9470d020fcbdff23ce1ce9844eca46a
Usman Qadri <qadri_usman@bah.com>
2022-09-20 16:22:59 -0400

    removed exportLoading since it doesn't do anything

--- frontend/src/components/modules/jbook/jbookViewHeaderHandler.js
+++ frontend/src/components/modules/jbook/jbookViewHeaderHandler.js
@@ -116,7 +116,6 @@ const JbookViewHeaderHandler = (props) => {
 		currentOrder,
 		sortSelected,
 		searchText,
-		exportLoading,
 		runSearch,
 	} = state;
 
@@ -479,23 +478,14 @@ const JbookViewHeaderHandler = (props) => {
 						}
 					}}
 				>
-					{!exportLoading ? (
-						<img
-							src={ExportIcon}
-							style={{
-								margin: '0 0 3px 3px',
-								width: 15,
-							}}
-							alt="export"
-						/>
-					) : (
-						<CircularProgress
-							color="#515151"
-							size={25}
-							style={{ display: 'flex', justifyContent: 'center' }}
-						/>
-					)}
-					{/* <img src={ExportIcon} style={{ margin: '0 0 3px 5px', width: 20, opacity: !mainPageData || (mainPageData.docs && mainPageData.docs.length <= 0) ? .6 : 1 }} alt="export"/> */}
+					<img
+						src={ExportIcon}
+						style={{
+							margin: '0 0 3px 3px',
+							width: 15,
+						}}
+						alt="export"
+					/>
 				</GCButton>
 			</div>
 			<FilterList
