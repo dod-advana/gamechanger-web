5d37c5cec01cbb0f463f0dd6e96aaf4b13ca0de0
616836 <david.rospond@gmail.com>
2022-10-13 11:55:57 -0400

    fixed typos

--- frontend/src/components/analystTools/tutotialSteps.js
+++ frontend/src/components/analystTools/tutotialSteps.js
@@ -96,14 +96,14 @@ const dctTutorialSteps = [
 		target: '.dct-tutorial-step-4',
 		title: 'Paragraph Input',
 		content:
-			"Continue to view the the text used for the query in the paragraph input pane.​ If more than one paragraph is entered in the query each paragraph will be assessed separately. Click on a different input to view it's results",
+			'Continue to view the the text used for the query in the paragraph input pane.​ If more than one paragraph is entered in the query, each paragraph will be assessed separately. Click on a different input to view its results',
 	},
 	{
 		...tutorialObject,
 		target: '.dct-tutorial-step-5',
 		title: 'Combine Paragraph Input',
 		content:
-			'Paragraph inputs can be combined to re-compare as a whole paragraph. First, select paragraphs by click the check mark next to the inputs to combine. Then, press the combine button to merge the selected paragraphs and reinitiate the comparison search with the combined text.',
+			'Paragraph inputs can be combined to re-compare as a whole paragraph. First, select paragraphs by clicking the check mark next to the inputs to combine. Then, press the combine button to merge the selected paragraphs and reinitiate the comparison search with the combined text.',
 		clickButton: true,
 		buttonID: 'first-dct-result',
 	},
@@ -119,7 +119,7 @@ const dctTutorialSteps = [
 		target: '.dct-tutorial-step-7',
 		title: 'Select a Document',
 		content:
-			'Select a document from the results to display the section of the document that is  similar to your query text. In addition, view the page number it was found on and its’ similarity score. The results of the document can also be exported here, or the document can be saved to favorites.',
+			'Select a document from the results to display the section of the document that is  similar to your query text. In addition, view the page number it was found on and its similarity score. The results of the document can also be exported here, or the document can be saved to favorites.',
 	},
 	{
 		...tutorialObject,
@@ -131,13 +131,13 @@ const dctTutorialSteps = [
 		...tutorialObject,
 		target: '.dct-tutorial-step-9',
 		title: 'Sort',
-		content: 'Sort results by selecting and option from the drop-down menu.',
+		content: 'Sort results by selecting an option from the drop-down menu.',
 	},
 	{
 		...tutorialObject,
 		target: '.dct-tutorial-step-10',
 		title: 'Export',
-		content: 'Export the results of the search by selecting the orange export icon',
+		content: 'Export the results of the search by selecting the orange export icon.',
 	},
 ];
 
