2e319f0f4f6b5196ae9723f5672f29bc3044c8e4
616836 <david.rospond@gmail.com>
2022-11-04 11:28:22 -0400

    remove local resp sql

--- backend/start.sh
+++ backend/start.sh
@@ -5,5 +5,4 @@ sequelize --options-path ./.sequelize-game_changer --env game_changer db:migrate
 sequelize --options-path ./.sequelize-gc-orchestration --env gc_orchestration db:migrate --url "postgresql://postgres:password@web-postgres/gc-orchestration" && \
 psql -U postgres -h web-postgres -d game_changer -f /usr/src/app/node_app/init/create_clones.sql && \
 psql -U postgres -h web-postgres -d game_changer -f /usr/src/app/node_app/init/create_admins.sql && \
-psql -U postgres -h web-postgres -d game_changer -f /usr/src/app/node_app/init/pop_mini_RE.sql && \
 nodemon -x 'sleep 1 && node --inspect=0.0.0.0:9229 index.js || (sleep 10; touch index.js)'
