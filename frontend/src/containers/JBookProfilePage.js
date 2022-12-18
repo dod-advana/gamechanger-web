27b523984741a7b1f292bd3bfbe3b9a5f8c309df
Usman Qadri <qadri_usman@bah.com>
2022-12-18 09:46:19 -0500

    added desired textfields

--- frontend/src/containers/JBookProfilePage.js
+++ frontend/src/containers/JBookProfilePage.js
@@ -237,7 +237,7 @@ const JBookProfilePage = () => {
 		}
 	};
 
-	// grab all profile page relaetd data
+	// grab all profile page related data
 	const getAllBYProjectData = async (id, year, portfolioName) => {
 		let allBYProjectData;
 		const currentUserData = await gameChangerUserAPI.getUserProfileData();
@@ -433,12 +433,12 @@ const JBookProfilePage = () => {
 					const tmpMatches = descriptions.value.match(regex);
 					matches = matches.concat(tmpMatches);
 				}
-
+				matches = [...new Set(matches)];
 				if (matches) {
 					for (const match of matches) {
 						descriptions.value = descriptions.value.replaceAll(
 							match,
-							`<span style="background-color: #1C2D64; color: white; padding: 0 4px;">${match}</span>`
+							`<span style="background-color: #1C2D64; color: white; padding: 0 4px; margin: 0 2px;">${match}</span>`
 						);
 					}
 				}
@@ -466,11 +466,12 @@ const JBookProfilePage = () => {
 						const tmpMatches = accompObject.Value.match(regex);
 						matches = matches.concat(tmpMatches);
 					});
+					matches = [...new Set(matches)];
 					if (matches && matches.length > 0 && matches !== null) {
 						for (const match of matches) {
 							accompObject.Value = accompObject.Value.replaceAll(
 								match,
-								`<span style="background-color: ${'#1C2D64'}; color: white; padding: 0 4px;">${match}</span>`
+								`<span style="background-color: ${'#1C2D64'}; color: white; padding: 0 4px; margin: 0 2px;">${match}</span>`
 							);
 						}
 					}
@@ -499,10 +500,11 @@ const JBookProfilePage = () => {
 
 	const applyHighlighting = useCallback(
 		(textObject, keywordBoxes, matches, keyword, property) => {
-			for (const match of matches) {
+			const uniqueMatches = [...new Set(matches)];
+			for (const match of uniqueMatches) {
 				textObject[property] = textObject[property].replaceAll(
 					match,
-					`<span style="background-color: ${gcOrange}; color: white; padding: 0 4px;">${match}</span>`
+					`<span style="background-color: ${gcOrange}; color: white; padding: 0 4px; margin: 0 2px;">${match}</span>`
 				);
 
 				if (!isCheckboxSet && keywordBoxes.indexOf(keyword) === -1) {
@@ -1065,7 +1067,6 @@ const JBookProfilePage = () => {
 			let latestReviewer;
 			if (reviewData.pocClassLabel) {
 				latestReviewer = reviewData.servicePOCName || 'The POC Reviewer';
-				console.log('latestReviewer: ', latestReviewer);
 			} else if (reviewData.serviceClassLabel) {
 				latestReviewer = reviewData.serviceReviewer || 'The RAI Lead Reviewer';
 			} else {
