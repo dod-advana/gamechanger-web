ae63bc1b3ef0b46ef2209cc3f599d77f079268f5
Usman Qadri <qadri_usman@bah.com>
2022-12-18 09:46:19 -0500

    added desired textfields

--- frontend/src/components/modules/jbook/profilePage/jbookProfilePageHelper.js
+++ frontend/src/components/modules/jbook/profilePage/jbookProfilePageHelper.js
@@ -208,6 +208,7 @@ const Metadata = ({ budgetType, keywordCheckboxes, setKeywordCheck }) => {
 };
 
 const ProjectDescription = ({ profileLoading, projectData, programElement, projectNum, projectDescriptions }) => {
+	console.log('here is projectDescriptions', projectDescriptions);
 	return (
 		<>
 			{profileLoading ? (
@@ -223,27 +224,29 @@ const ProjectDescription = ({ profileLoading, projectData, programElement, proje
 					</Typography>
 					<div style={{ overflow: 'auto' }} data-cy="jbook-project-descriptions">
 						<Typography variant="subtitle1" style={{ fontSize: '16px', margin: '10px 0' }}>
-							{projectDescriptions.map((pd) => {
-								return (
-									<>
-										<Typography
-											key={pd.title}
-											variant="h3"
-											style={{ fontWeight: 'bold', width: '100%' }}
-										>
-											{pd.title}
-										</Typography>
-										<blockquote
-											style={{ borderLeft: 'none' }}
-											dangerouslySetInnerHTML={{
-												__html: sanitizeHtml(pd.value, {
-													allowedAttributes: { span: ['style'] },
-												}),
-											}}
-										/>
-									</>
-								);
-							})}
+							{projectDescriptions
+								.filter((pd) => pd.value !== 'N/A' && pd.value !== 'Not applicable for this item.')
+								.map((pd) => {
+									return (
+										<>
+											<Typography
+												key={pd.title}
+												variant="h3"
+												style={{ fontWeight: 'bold', width: '100%' }}
+											>
+												{pd.title}
+											</Typography>
+											<blockquote
+												style={{ borderLeft: 'none' }}
+												dangerouslySetInnerHTML={{
+													__html: sanitizeHtml(pd.value, {
+														allowedAttributes: { span: ['style'], div: ['style'] },
+													}),
+												}}
+											/>
+										</>
+									);
+								})}
 						</Typography>
 					</div>
 				</>
@@ -295,7 +298,7 @@ const aggregateProjectDescriptions = (projectData) => {
 		projectMissionDescription: {
 			title: projectData.budgetType === 'rdoc' ? 'Program Mission Description' : 'Description',
 		},
-		missionDescBudgetJustification: { title: 'Project Description' },
+		missionDescBudgetJustification: { title: 'Justification' },
 		SubProj_Title: { title: 'Sub-project Title' },
 		Adj_OtherAdj_Title: { title: 'Other Title' },
 		CongAdds_Title: { title: 'CongAdds Title' },
@@ -325,6 +328,9 @@ const aggregateProjectDescriptions = (projectData) => {
 		'P3a-20_Milestone_Desc': { title: 'P3a-20_Milestone_Desc' },
 		p3a_dev_milestones_n: { title: 'P3a Milestones' },
 		p3a_contract_data_n: { title: 'P3a Contract Titles' },
+		otherProgramFundSummaryRemarks: { title: 'Other Summary Remarks' },
+		r4a_schedule_details_n: { title: 'Schedule Details' },
+		projectNotes: { title: 'Project Notes' },
 
 		// odoc
 		budgetLineItemTitle: { title: 'Budget Line Item Title' },
@@ -344,10 +350,21 @@ const aggregateProjectDescriptions = (projectData) => {
 		titleMapping['projectPerformanceMetrics'] = { title: 'Project Performance Metrics' };
 		titleMapping['otherProgramFundingSummaryRemarks'] = { title: 'Other Program Funding Summary Remarks' };
 	}
-
 	Object.keys(titleMapping).forEach((key) => {
 		if (projectData[key]) {
 			let obj = { ...titleMapping[key], value: projectData[key] };
+			if (key === 'r4a_schedule_details_n') {
+				obj.value.sort((a, b) => new Date(b.toComplete_s) - new Date(a.toComplete_s));
+				let str = `<div style='display: flex; justify-content: space-between'>`;
+				str += `<div style='display: flex; flex-direction: column'>`;
+				obj.value.map((d) => d.Event_Title_t).forEach((event) => (str += `<p>${event}</p>`));
+				str += `</div>`;
+				str += `<div style='display: flex; flex-direction: column;'>`;
+				obj.value.map((d) => d.toComplete_s).forEach((date) => (str += `<p>${date}</p>`));
+				str += `</div>`;
+				str += '</div>';
+				obj.value = str;
+			}
 			if (key === 'p3a_dev_milestones_n' || key === 'p3a_contract_data_n') {
 				let str = ``;
 				for (let obj of projectData[key]) {
