d1f667240351c9eb1e88eba8c66d456d6803498d
Usman Qadri <qadri_usman@bah.com>
2023-01-12 13:53:21 -0500

    another optimization

--- frontend/src/components/modules/policy/policyCardHandler.js
+++ frontend/src/components/modules/policy/policyCardHandler.js
@@ -363,10 +363,13 @@ const StyledFrontCardContent = styled.div`
 
 		.page-hits {
 			min-width: 100px;
-			max-height: 160px;
+			height: fit-content;
+			max-height: 150px;
 			overflow: auto;
 			border: 1px solid rgb(189, 189, 189);
 			border-top: 0px;
+			position: relative;
+			z-index: 1;
 
 			.page-hit {
 				display: flex;
