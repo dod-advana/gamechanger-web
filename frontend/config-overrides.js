f8324b272da27db9bcfcd477bf653e5975dd6846
Pete Sheurpukdi <sheurpukdi_pete@bah.com>
2023-02-02 11:00:12 -0800

    try different config-overrides

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,14 +1,14 @@
-const webpack = require('webpack');
 module.exports = function override(config, env) {
-  config.resolve.fallback = {
-    https: require.resolve('https-browserify'),
-  };
-  config.plugins.push(
-    new webpack.ProvidePlugin({
-      process: 'process/browser',
-      Buffer: ['buffer', 'Buffer'],
-    }),
-  );
+  // New config, e.g. config.plugins.push...
 
-  return config;
+  config.module.rules = [...config.module.rules,
+  {
+    test: /\.m?js/,
+    resolve: {
+      fullySpecified: false
+    }
+  }
+  ]
+
+  return config
 }
