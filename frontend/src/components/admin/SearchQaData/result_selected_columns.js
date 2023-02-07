4fcfa633ed646ef46df8ec4f3021d09dc7d41cf4
Segerlu <111815126+Segerlu@users.noreply.github.com>
2023-02-07 09:55:24 -0500

    Adds the GUI for viewing search performance tests

---
+++ frontend/src/components/admin/SearchQaData/result_selected_columns.js
@@ -0,0 +1,32 @@
+import 'react-table/react-table.css';
+import { TableRow } from '../util/GCAdminStyles';
+
+const RESULT_SELECTED_COLUMNS = [
+	{
+		Header: 'Source',
+		accessor: 'source',
+		Cell: (row) => <TableRow>{row.value && row.value !== null ? row.value : 'no source'}</TableRow>,
+	},
+	{
+		Header: 'Documents Tested',
+		accessor: 'number_of_documents_tested',
+		Cell: (row) => <TableRow>{row.value && row.value !== null ? row.value : 0}</TableRow>,
+	},
+	{
+		Header: 'Documents Missed',
+		accessor: 'number_of_documents_not_found',
+		Cell: (row) => <TableRow>{row.value && row.value !== null ? row.value : 0}</TableRow>,
+	},
+	{
+		Header: 'Documents Found',
+		accessor: 'number_of_documents_found',
+		Cell: (row) => <TableRow>{row.value && row.value !== null ? row.value : 0}</TableRow>,
+	},
+	{
+		Header: 'Average Position',
+		accessor: 'average_position',
+		Cell: (row) => <TableRow>{row.value && row.value !== null ? row.value : 0}</TableRow>,
+	},
+];
+
+export default RESULT_SELECTED_COLUMNS;
