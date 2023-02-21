2df6c9802b03e6d081a3a72c5eee3277c793e5d0
Karolyn Babalola <babalola_karolyn@bah.com>
2023-02-21 17:32:14 -0500

    Changed department column name to match spreadsheet upload (for simplicity).

--- backend/node_app/sql/v13-update-review-dept-code-column-refid.sql
+++ backend/node_app/sql/v13-update-review-dept-code-column-refid.sql
@@ -22,10 +22,10 @@ WHERE rd.budget_type='odoc' AND (lower(rd.agency) like CONCAT('%',lower(jd.uot_d
 
 --MIGRATION TO ADD DEPT CODE TO review TABLE
 
--- ALTER TABLE public.review ADD COLUMN IF NOT EXISTS dept_code TEXT;
+-- ALTER TABLE public.review ADD COLUMN IF NOT EXISTS department TEXT;
 
 UPDATE public.review rd
-SET dept_code = jd.uot_department
+SET jd.department = jd.uot_department
 FROM jbooklookup jd
 WHERE lower(rd.agency) like CONCAT('%',lower(jd.uot_department_desc),'%') OR lower(rd.agency) like CONCAT('%',lower(jd.uot_agency),'%') OR lower(rd.agency) like CONCAT('%',lower(jd.org_code),'%');
 
